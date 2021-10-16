ce54bee2d136226e862b62e8a85affef01c228c0
yaolianhua <ylh789@qq.com>
2021-10-16 21:19:10 +0800

    commit format test

--- hotCloud-core/hotCloud-core-kubernetes/src/main/java/io/hotCloud/core/kubernetes/affinity/NodeAffinity.java
+++ hotCloud-core/hotCloud-core-kubernetes/src/main/java/io/hotCloud/core/kubernetes/affinity/NodeAffinity.java
@@ -1,6 +1,5 @@
 package io.hotCloud.core.kubernetes.affinity;
 
-import lombok.Builder;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -10,10 +9,8 @@ import java.util.List;
  * @author yaolianhua789@gmail.com
  **/
 @Data
-@Builder
 public class NodeAffinity {
 
-    @Builder.Default
     private List<PreferredSchedulingTerm> preferredDuringSchedulingIgnoredDuringExecution = new ArrayList<>();
 
     private NodeSelector requiredDuringSchedulingIgnoredDuringExecution;
