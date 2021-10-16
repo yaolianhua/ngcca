b951593c71dd10d3cfe05d49ceb55ab0473cd001
yaolianhua <ylh789@qq.com>
2021-10-16 21:35:47 +0800

    commit format test

--- hotCloud-core/hotCloud-core-kubernetes/src/main/java/io/hotCloud/core/kubernetes/affinity/NodeSelector.java
+++ hotCloud-core/hotCloud-core-kubernetes/src/main/java/io/hotCloud/core/kubernetes/affinity/NodeSelector.java
@@ -1,6 +1,5 @@
 package io.hotCloud.core.kubernetes.affinity;
 
-import lombok.Builder;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -10,10 +9,8 @@ import java.util.List;
  * @author yaolianhua789@gmail.com
  **/
 @Data
-@Builder
 public class NodeSelector {
 
-    @Builder.Default
     private List<NodeSelectorTerm> nodeSelectorTerms = new ArrayList<>();
 
 }
