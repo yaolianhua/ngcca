286a6c73a8b03d33957aa68157a0e6a39d422448
yaolianhua <yaolianhua789@gmail.com>
2021-10-17 18:15:05 +0800

    added probe api

---
+++ hotCloud-server/src/main/java/io/hotCloud/server/ProbeController.java
@@ -0,0 +1,26 @@
+package io.hotCloud.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@Slf4j
+public class ProbeController {
+
+    @GetMapping(value = "/livez")
+    public ResponseEntity<Void> livenessProbe() {
+        log.debug("livenessProbe check successful");
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
+    }
+
+    @GetMapping(value = "/readyz")
+    public ResponseEntity<Void> readinessProbe() {
+        log.debug("readinessProbe check successful");
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
+    }
+}
