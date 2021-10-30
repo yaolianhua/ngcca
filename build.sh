42007b8bef4bbbbb3e6c655a429df9ab6f9c32a4
yaolianhua <yaolianhua789@gmail.com>
2021-10-30 18:39:10 +0800

    update harbor registry

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ BUILD_TIMESTAMP=$(date '+%Y%m%d%H%M%S')
 echo "Using hotCloud version: ${HOTCLOUD_VERSION}"
 # Build image
 
-IMAGE="yaolianhua/hotcloud:${HOTCLOUD_VERSION}.${BUILD_TIMESTAMP}"
+IMAGE="harbor.local:7000/hotcloud/hotcloud:${HOTCLOUD_VERSION}.${BUILD_TIMESTAMP}"
 docker build -f Dockerfile --build-arg HOTCLOUD_VERSION="${HOTCLOUD_VERSION}" -t "${IMAGE}" .
+docker login harbor.local:7000 -u admin -p Harbor12345
 docker push "${IMAGE}"
 
 echo "${IMAGE} pushed successful!"
