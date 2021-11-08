4ca24fd5ee48b6749c293d9409d484849d24222c
yaolianhua <yaolianhua789@gmail.com>
2021-11-08 14:12:08 +0800

    refactor test

---
+++ hotCloud-server/src/test/java/io/hotcloud/HotCloudApplicationTest.java
@@ -0,0 +1,11 @@
+package io.hotcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yaolianhua789@gmail.com
+ **/
+@SpringBootApplication
+public class HotCloudApplicationTest {
+    //
+}
