d875f32f9a3d15fdc2b27dc5d92502eebc54f104
yaolianhua <yaolianhua789@gmail.com>
2021-12-11 19:30:22 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 HOTCLOUD_VERSION="0.1"
 BUILD_TIMESTAMP=$(date '+%Y%m%d%H%M%S')
 TAG="${HOTCLOUD_VERSION}.${BUILD_TIMESTAMP}"
-echo "Using hotCloud version: ${TAG}"
+echo "Using hotCloud tag: ${TAG}"
 # Build image
 IMAGE="yaolianhua/hotcloud:${TAG}"
 docker build -f Dockerfile --build-arg HOTCLOUD_VERSION="${HOTCLOUD_VERSION}" -t "${IMAGE}" .
