3b46c4264afff5b88a7d8ae595d8dce187e5f5e4
yaolianhua <yaolianhua789@gmail.com>
2021-12-15 13:58:50 +0800

    add pod log

---
+++ hotCloud-server/src/main/java/io/hotcloud/server/kubernetes/controller/PodLogController.java
@@ -0,0 +1,41 @@
+package io.hotcloud.server.kubernetes.controller;
+
+import io.hotcloud.core.common.Result;
+import io.hotcloud.core.kubernetes.pod.PodLogFetchApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static io.hotcloud.server.WebResponse.ok;
+
+/**
+ * @author yaolianhua789@gmail.com
+ **/
+@RestController
+@RequestMapping("/v1/kubernetes/pods")
+public class PodLogController {
+
+
+    private final PodLogFetchApi podLogFetchApi;
+
+    public PodLogController(PodLogFetchApi podLogFetchApi) {
+        this.podLogFetchApi = podLogFetchApi;
+    }
+
+    @GetMapping("/{namespace}/{pod}/log")
+    public ResponseEntity<Result<String>> podlogs(@PathVariable String namespace,
+                                                  @PathVariable String pod,
+                                                  @RequestParam(value = "tail", required = false) Integer tailing) {
+        String log = podLogFetchApi.getLog(namespace, pod, tailing);
+        return ok(log);
+    }
+
+    @GetMapping("/{namespace}/{pod}/loglines")
+    public ResponseEntity<Result<List<String>>> podloglines(@PathVariable String namespace,
+                                                            @PathVariable String pod,
+                                                            @RequestParam(value = "tail", required = false) Integer tailing) {
+        List<String> lines = podLogFetchApi.getLogLines(namespace, pod, tailing);
+        return ok(lines);
+    }
+}
