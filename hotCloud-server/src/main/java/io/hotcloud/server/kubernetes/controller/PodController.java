676c396bc3d0ba508ee7a16aa3b5b7c8f34d66b1
yaolianhua <yaolianhua789@gmail.com>
2021-12-18 15:25:58 +0800

    add pod create && test

--- hotCloud-server/src/main/java/io/hotcloud/server/kubernetes/controller/PodController.java
+++ hotCloud-server/src/main/java/io/hotcloud/server/kubernetes/controller/PodController.java
@@ -1,13 +1,19 @@
 package io.hotcloud.server.kubernetes.controller;
 
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodList;
 import io.hotcloud.core.common.Result;
-import io.hotcloud.core.kubernetes.pod.PodLogFetchApi;
+import io.hotcloud.core.kubernetes.YamlBody;
+import io.hotcloud.core.kubernetes.pod.*;
+import io.kubernetes.client.openapi.ApiException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
-import static io.hotcloud.server.WebResponse.ok;
+import static io.hotcloud.server.WebResponse.*;
 
 /**
  * @author yaolianhua789@gmail.com
@@ -18,9 +24,15 @@ public class PodController {
 
 
     private final PodLogFetchApi podLogFetchApi;
+    private final PodCreateApi podCreateApi;
+    private final PodReadApi podReadApi;
+    private final PodDeleteApi podDeleteApi;
 
-    public PodController(PodLogFetchApi podLogFetchApi) {
+    public PodController(PodLogFetchApi podLogFetchApi, PodCreateApi podCreateApi, PodReadApi podReadApi, PodDeleteApi podDeleteApi) {
         this.podLogFetchApi = podLogFetchApi;
+        this.podCreateApi = podCreateApi;
+        this.podReadApi = podReadApi;
+        this.podDeleteApi = podDeleteApi;
     }
 
     @GetMapping("/{namespace}/{pod}/log")
@@ -37,5 +49,39 @@ public class PodController {
                                                             @RequestParam(value = "tail", required = false) Integer tailing) {
         List<String> lines = podLogFetchApi.getLogLines(namespace, pod, tailing);
         return ok(lines);
+    }
+
+    @PostMapping
+    public ResponseEntity<Result<Pod>> pod(@Validated @RequestBody PodCreateParams params) throws ApiException {
+        Pod pod = podCreateApi.pod(params);
+        return created(pod);
+    }
+
+    @PostMapping("/yaml")
+    public ResponseEntity<Result<Pod>> pod(@RequestBody YamlBody yaml) throws ApiException {
+        Pod pod = podCreateApi.pod(yaml.getYaml());
+        return created(pod);
+    }
+
+
+    @GetMapping("/{namespace}/{pod}")
+    public ResponseEntity<Result<Pod>> podRead(@PathVariable String namespace,
+                                               @PathVariable String pod) {
+        Pod read = podReadApi.read(namespace, pod);
+        return ok(read);
+    }
+
+    @GetMapping("/{namespace}")
+    public ResponseEntity<Result<PodList>> podListRead(@PathVariable String namespace,
+                                                       @RequestParam(required = false) Map<String, String> labelSelector) {
+        PodList list = podReadApi.read(namespace, labelSelector);
+        return ok(list);
+    }
+
+    @DeleteMapping("/{namespace}/{pod}")
+    public ResponseEntity<Result<Void>> podDelete(@PathVariable("namespace") String namespace,
+                                                  @PathVariable("pod") String name) throws ApiException {
+        podDeleteApi.delete(namespace, name);
+        return accepted();
     }
 }
