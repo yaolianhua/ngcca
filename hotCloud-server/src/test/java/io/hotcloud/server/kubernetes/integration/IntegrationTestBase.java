c3f63a41f0f3fd4ed4e7451281c470aa429c5d85
yaolianhua <yaolianhua789@gmail.com>
2021-12-19 15:37:10 +0800

    format

--- hotCloud-server/src/test/java/io/hotcloud/server/kubernetes/integration/IntegrationTestBase.java
+++ hotCloud-server/src/test/java/io/hotcloud/server/kubernetes/integration/IntegrationTestBase.java
@@ -7,7 +7,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author yaolianhua789@gmail.com
- * <p>
  * <p/>
  * Ensure that there is a configuration {@code $HOME/.kube/config} that can access the k8s cluster locally
  **/
