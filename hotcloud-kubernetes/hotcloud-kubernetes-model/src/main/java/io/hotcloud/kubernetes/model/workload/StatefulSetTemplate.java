d6ad885d88351e9ada293cf0bb1e2a70d73bf3fd
yaolianhua <yaolianhua789@gmail.com>
2022-01-04 13:55:17 +0800

    sts IT

--- hotcloud-kubernetes/hotcloud-kubernetes-model/src/main/java/io/hotcloud/kubernetes/model/workload/StatefulSetTemplate.java
+++ hotcloud-kubernetes/hotcloud-kubernetes-model/src/main/java/io/hotcloud/kubernetes/model/workload/StatefulSetTemplate.java
@@ -4,6 +4,8 @@ import io.hotcloud.kubernetes.model.ObjectMetadata;
 import io.hotcloud.kubernetes.model.pod.PodTemplateSpec;
 import lombok.Data;
 
+import javax.validation.Valid;
+
 /**
  * @author yaolianhua789@gmail.com
  **/
@@ -12,6 +14,7 @@ public class StatefulSetTemplate {
 
     private ObjectMetadata metadata = new ObjectMetadata();
 
+    @Valid
     private PodTemplateSpec spec = new PodTemplateSpec();
 
     public StatefulSetTemplate(ObjectMetadata metadata, PodTemplateSpec spec) {
