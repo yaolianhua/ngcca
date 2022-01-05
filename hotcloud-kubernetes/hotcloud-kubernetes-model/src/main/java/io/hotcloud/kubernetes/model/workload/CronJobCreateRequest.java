d6a32f7ceabc0d84c31912a945e431ce2d427c2c
yaolianhua <yaolianhua789@gmail.com>
2022-01-05 18:52:24 +0800

    cronjob IT

--- hotcloud-kubernetes/hotcloud-kubernetes-model/src/main/java/io/hotcloud/kubernetes/model/workload/CronJobCreateRequest.java
+++ hotcloud-kubernetes/hotcloud-kubernetes-model/src/main/java/io/hotcloud/kubernetes/model/workload/CronJobCreateRequest.java
@@ -3,6 +3,8 @@ package io.hotcloud.kubernetes.model.workload;
 import io.hotcloud.kubernetes.model.ObjectMetadata;
 import lombok.Data;
 
+import javax.validation.Valid;
+
 /**
  * @author yaolianhua789@gmail.com
  **/
@@ -11,5 +13,6 @@ public class CronJobCreateRequest {
 
     private ObjectMetadata metadata = new ObjectMetadata();
 
+    @Valid
     private CronJobSpec spec = new CronJobSpec();
 }
--- hotcloud-kubernetes/hotcloud-kubernetes-model/src/main/java/io/hotcloud/kubernetes/model/workload/CronJobSpec.java
+++ hotcloud-kubernetes/hotcloud-kubernetes-model/src/main/java/io/hotcloud/kubernetes/model/workload/CronJobSpec.java
@@ -2,6 +2,9 @@ package io.hotcloud.kubernetes.model.workload;
 
 import lombok.Data;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 /**
  * @author yaolianhua789@gmail.com
  **/
@@ -10,9 +13,11 @@ public class CronJobSpec {
 
     private String concurrencyPolicy;
     private Integer failedJobsHistoryLimit;
+    @NotNull(message = "spec.schedule is null")
     private String schedule;
     private Long startingDeadlineSeconds;
     private Integer successfulJobsHistoryLimit;
     private Boolean suspend;
-    private CronJobTemplate jobTemplate;
+    @Valid
+    private CronJobTemplate jobTemplate = new CronJobTemplate();
 }
