894bd7173aef83a5992a08697347aae4e1fff88b
yaolianhua <yaolianhua789@gmail.com>
2022-01-09 15:22:19 +0800

    update rabbitmq config

--- hotcloud-message/hotcloud-message-server/src/main/java/io/hotcloud/message/server/rabbitmq/RabbitmqConfiguration.java
+++ hotcloud-message/hotcloud-message-server/src/main/java/io/hotcloud/message/server/rabbitmq/RabbitmqConfiguration.java
@@ -2,9 +2,13 @@ package io.hotcloud.message.server.rabbitmq;
 
 import io.hotcloud.message.server.MessageProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -19,4 +23,41 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(RabbitmqProperties.class)
 @EnableRabbit
 public class RabbitmqConfiguration {
+
+    public static final String EXCHANGE_FANOUT_BROADCAST_MESSAGE = "hotcloud.message.broadcast";
+
+    public static final String QUEUE_SUBSCRIBE_MESSAGE = "hotcloud.message.subscribe";
+
+    @Bean
+    public FanoutExchange messageBroadcastExchange() {
+        return ExchangeBuilder.fanoutExchange(EXCHANGE_FANOUT_BROADCAST_MESSAGE)
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    public Queue messageSubscribeQueue() {
+        return QueueBuilder.durable(QUEUE_SUBSCRIBE_MESSAGE).build();
+    }
+
+    @Bean
+    public Binding binding(FanoutExchange messageBroadcastExchange,
+                           Queue messageSubscribeQueue) {
+        return BindingBuilder.bind(messageSubscribeQueue)
+                .to(messageBroadcastExchange);
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory,
+                                   FanoutExchange messageBroadcastExchange,
+                                   Queue messageSubscribeQueue) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setAutoStartup(true);
+        rabbitAdmin.declareQueue(messageSubscribeQueue);
+        log.info("Created default queue: {}", QUEUE_SUBSCRIBE_MESSAGE);
+        rabbitAdmin.declareExchange(messageBroadcastExchange);
+        log.info("Created default exchange: {}", EXCHANGE_FANOUT_BROADCAST_MESSAGE);
+        return rabbitAdmin;
+    }
+
 }
