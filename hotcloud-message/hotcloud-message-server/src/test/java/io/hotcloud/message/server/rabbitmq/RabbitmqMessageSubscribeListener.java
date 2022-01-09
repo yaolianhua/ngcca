89dc5b5ac21bd348bc9beb6cda516df91cb68380
yaolianhua <yaolianhua789@gmail.com>
2022-01-09 15:23:32 +0800

    add rabbitmq IT

---
+++ hotcloud-message/hotcloud-message-server/src/test/java/io/hotcloud/message/server/rabbitmq/RabbitmqMessageSubscribeListener.java
@@ -0,0 +1,29 @@
+package io.hotcloud.message.server.rabbitmq;
+
+import io.hotcloud.message.api.Message;
+import io.hotcloud.message.api.MessageBody;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+/**
+ * @author yaolianhua789@gmail.com
+ **/
+@Slf4j
+public class RabbitmqMessageSubscribeListener {
+
+    @RabbitListener(
+            bindings = {
+                    @QueueBinding(
+                            value = @Queue(value = RabbitmqConfiguration.QUEUE_SUBSCRIBE_MESSAGE),
+                            exchange = @Exchange(type = ExchangeTypes.FANOUT, value = RabbitmqConfiguration.EXCHANGE_FANOUT_BROADCAST_MESSAGE)
+                    )
+            }
+    )
+    public void subscribe(Message<MessageBody> message) {
+        log.info("Received message: \n {}", message);
+    }
+}
