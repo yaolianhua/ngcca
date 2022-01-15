0e5697f514ec107a459e3966a49149aede1ee6af
Jason <yaolianhua789@gmail.com>
2022-01-15 11:39:06 +0800

    message optimization

--- hotcloud-message/hotcloud-message-api/src/main/java/io/hotcloud/message/api/MessageBroadcaster.java
+++ hotcloud-message/hotcloud-message-api/src/main/java/io/hotcloud/message/api/MessageBroadcaster.java
@@ -3,6 +3,7 @@ package io.hotcloud.message.api;
 /**
  * @author yaolianhua789@gmail.com
  **/
+@FunctionalInterface
 public interface MessageBroadcaster {
 
     <T> void broadcast(Message<T> message);
