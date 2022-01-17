3a1f3fd0be9aa079438c9bb7d44c665c52115cf9
Jason <yaolianhua789@gmail.com>
2022-01-17 13:24:17 +0800

    optimization

--- hotcloud-kubernetes/hotcloud-kubernetes-server/src/test/java/io/hotcloud/kubernetes/HotCloudKubernetesApplicationTest.java
+++ hotcloud-kubernetes/hotcloud-kubernetes-server/src/test/java/io/hotcloud/kubernetes/HotCloudKubernetesApplicationTest.java
@@ -8,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  **/
 @SpringBootApplication(
         scanBasePackageClasses = {
-                MessageConfiguration.class
+                MessageConfiguration.class,
+                HotCloudKubernetesApplicationTest.class
         }
 )
 public class HotCloudKubernetesApplicationTest {
