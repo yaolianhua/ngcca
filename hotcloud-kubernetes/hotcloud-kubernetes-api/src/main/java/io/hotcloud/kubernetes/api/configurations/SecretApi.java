d0478839aca396c58f96df868f4adde3d07dd68a
Jason <yaolianhua789@gmail.com>
2022-03-13 14:05:38 +0800

    doc

--- hotcloud-kubernetes/hotcloud-kubernetes-api/src/main/java/io/hotcloud/kubernetes/api/configurations/SecretApi.java
+++ hotcloud-kubernetes/hotcloud-kubernetes-api/src/main/java/io/hotcloud/kubernetes/api/configurations/SecretApi.java
@@ -15,17 +15,44 @@ import java.util.Objects;
  * @author yaolianhua789@gmail.com
  **/
 public interface SecretApi {
-
+    /**
+     * Create Secret from {@code SecretCreateRequest}
+     *
+     * @param request {@link SecretCreateRequest}
+     * @return {@link Secret}
+     * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
+     */
     default Secret secret(SecretCreateRequest request) throws ApiException {
         V1Secret v1Secret = SecretBuilder.build(request);
         String json = Yaml.dump(v1Secret);
         return this.secret(json);
     }
 
+    /**
+     * Create Secret from yaml
+     *
+     * @param yaml kubernetes yaml string
+     * @return {@link Secret}
+     * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
+     */
     Secret secret(String yaml) throws ApiException;
 
+    /**
+     * Delete namespaced Secret
+     *
+     * @param namespace namespace
+     * @param secret    secret name
+     * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
+     */
     void delete(String namespace, String secret) throws ApiException;
 
+    /**
+     * Read namespaced Secret
+     *
+     * @param namespace namespace
+     * @param secret    secret name
+     * @return {@link Secret}
+     */
     default Secret read(String namespace, String secret) {
         SecretList secretList = this.read(namespace);
         return secretList.getItems()
@@ -35,13 +62,31 @@ public interface SecretApi {
                 .orElse(null);
     }
 
+    /**
+     * Read SecretList all namespace
+     *
+     * @return {@link SecretList}
+     */
     default SecretList read() {
         return this.read(null);
     }
 
+    /**
+     * Read namespaced SecretList
+     *
+     * @param namespace namespace
+     * @return {@link Secret}
+     */
     default SecretList read(String namespace) {
         return this.read(namespace, Collections.emptyMap());
     }
 
+    /**
+     * Read namespaced SecretList
+     *
+     * @param namespace     namespace
+     * @param labelSelector label selector
+     * @return {@link SecretList}
+     */
     SecretList read(String namespace, Map<String, String> labelSelector);
 }
