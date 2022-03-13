f092e5e0fa38e06ef6322b088309fed66bd64696
Jason <yaolianhua789@gmail.com>
2022-03-13 14:15:17 +0800

    doc

--- hotcloud-kubernetes/hotcloud-kubernetes-api/src/main/java/io/hotcloud/kubernetes/api/workload/CronJobApi.java
+++ hotcloud-kubernetes/hotcloud-kubernetes-api/src/main/java/io/hotcloud/kubernetes/api/workload/CronJobApi.java
@@ -15,17 +15,44 @@ import java.util.Objects;
  * @author yaolianhua789@gmail.com
  **/
 public interface CronJobApi {
-
+    /**
+     * Create CronJob from {@code CronJobCreateRequest}
+     *
+     * @param request {@link CronJobCreateRequest}
+     * @return {@link CronJob}
+     * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
+     */
     default CronJob cronjob(CronJobCreateRequest request) throws ApiException {
         V1CronJob v1CronJob = CronJobBuilder.build(request);
         String json = Yaml.dump(v1CronJob);
         return this.cronjob(json);
     }
 
+    /**
+     * Create CronJob from yaml
+     *
+     * @param yaml kubernetes yaml string
+     * @return {@link CronJob}
+     * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
+     */
     CronJob cronjob(String yaml) throws ApiException;
 
+    /**
+     * Delete namespaced CronJob
+     *
+     * @param namespace namespace
+     * @param cronjob   cronJob name
+     * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
+     */
     void delete(String namespace, String cronjob) throws ApiException;
 
+    /**
+     * Read namespaced CronJob
+     *
+     * @param namespace namespace
+     * @param cronjob   cronJob name
+     * @return {@link CronJob}
+     */
     default CronJob read(String namespace, String cronjob) {
         CronJobList cronJobList = this.read(namespace);
         return cronJobList.getItems()
@@ -35,13 +62,31 @@ public interface CronJobApi {
                 .orElse(null);
     }
 
+    /**
+     * Read CronJobList all namespace
+     *
+     * @return {@link CronJobList}
+     */
     default CronJobList read() {
         return this.read(null);
     }
 
+    /**
+     * Read namespaced CronJobList
+     *
+     * @param namespace namespace
+     * @return {@link CronJobList}
+     */
     default CronJobList read(String namespace) {
         return this.read(namespace, Collections.emptyMap());
     }
 
+    /**
+     * Read namespaced CronJobList
+     *
+     * @param namespace     namespace
+     * @param labelSelector label selector
+     * @return {@link CronJobList}
+     */
     CronJobList read(String namespace, Map<String, String> labelSelector);
 }
