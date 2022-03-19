dcb835a170885cc990858d76cf98c40173c5f88b
yaolianhua <yaolianhua789@gmail.com>
2022-03-19 15:17:18 +0800

    test

---
+++ hotcloud-buildpack/hotcloud-buildpack-server/src/test/java/io/hotcloud/buildpack/HotCloudBuildPackApplicationTest.java
@@ -0,0 +1,11 @@
+package io.hotcloud.buildpack;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yaolianhua789@gmail.com
+ **/
+@SpringBootApplication(scanBasePackageClasses = HotCloudBuildPackApplicationTest.class)
+public class HotCloudBuildPackApplicationTest {
+    //
+}
---
+++ hotcloud-buildpack/hotcloud-buildpack-server/src/test/java/io/hotcloud/buildpack/server/controller/GitControllerTest.java
@@ -0,0 +1,50 @@
+package io.hotcloud.buildpack.server.controller;
+
+import io.hotcloud.buildpack.git.GitApi;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author yaolianhua789@gmail.com
+ **/
+@WebMvcTest(value = GitController.class)
+@MockBeans(value = {
+        @MockBean(classes = {
+                GitApi.class
+        })
+})
+public class GitControllerTest {
+
+    public final static String PATH = "/v1/git";
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private GitApi gitApi;
+
+    @Test
+    public void cloneRepository() throws Exception {
+
+        LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>(8);
+        params.set("gitUrl", "https://github.com/GoogleContainerTools/kaniko.git");
+        params.set("localPath", "localRepository");
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post(PATH.concat("/clone")).params(params))
+                .andDo(print())
+                .andExpect(status().isOk());
+        //was invoked one time
+        verify(gitApi, times(1)).clone("https://github.com/GoogleContainerTools/kaniko.git",
+                null, "localRepository", null, null);
+    }
+
+}
