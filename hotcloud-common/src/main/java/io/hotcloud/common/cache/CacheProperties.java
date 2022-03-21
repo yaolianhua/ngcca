408943dd5c2f3c4e58184de27b8b1238a319620e
Jason <yaolianhua789@gmail.com>
2022-03-21 17:28:13 +0800

    [common] redis cache

--- hotcloud-common/src/main/java/io/hotcloud/common/cache/CacheProperties.java
+++ hotcloud-common/src/main/java/io/hotcloud/common/cache/CacheProperties.java
@@ -1,5 +1,6 @@
 package io.hotcloud.common.cache;
 
+import io.hotcloud.common.Assert;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -17,18 +18,32 @@ public class CacheProperties {
     public static final String PROPERTIES_TYPE_NAME = "cache.type";
     private Type type = Type.local;
 
+    private RedisProperties redis;
+
     @PostConstruct
     public void print() {
         if (type == Type.local) {
             log.info("【Load Cache Configuration. implementation using Caffeine Cache】");
         }
         if (type == Type.redis) {
-            log.info("【Load Cache Configuration. implementation using Redis Cache】");
+            Assert.notNull(redis, "Redis configuration is null", 400);
+            log.info("【Load Cache Configuration. implementation using Redis Cache. url='{}', using database '{}'】", String.format("redis://%s:%s", redis.getHost(), redis.getPort()), redis.getDatabase());
         }
     }
 
     public enum Type {
         //
         local, redis
+    }
+
+    @Data
+    public static class RedisProperties {
+
+        private String host;
+        private Integer port = 6379;
+        private Integer database = 0;
+
+        private String password;
+
     }
 }
--- hotcloud-common/src/main/java/io/hotcloud/common/cache/RedisCache.java
+++ hotcloud-common/src/main/java/io/hotcloud/common/cache/RedisCache.java
@@ -1,7 +1,12 @@
 package io.hotcloud.common.cache;
 
+import io.hotcloud.common.Assert;
+import io.hotcloud.common.HotCloudException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.CollectionUtils;
 
+import java.util.Set;
 import java.util.concurrent.Callable;
 
 /**
@@ -10,9 +15,20 @@ import java.util.concurrent.Callable;
 @Slf4j
 public class RedisCache extends AbstractValueAdaptingCache {
 
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public RedisCache(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
     @Override
     protected Object lookup(String key) {
-        return null;
+        Assert.hasText(key, "Key is null", 400);
+        Object o = redisTemplate.opsForValue().get(key);
+        if (o == null) {
+            return null;
+        }
+        return fromStoreValue(o);
     }
 
     @Override
@@ -27,21 +43,45 @@ public class RedisCache extends AbstractValueAdaptingCache {
 
     @Override
     public void put(String key, Object value) {
-
+        Assert.hasText(key, "Key is null", 400);
+        Assert.notNull(value, "Value is null", 400);
+        redisTemplate.opsForValue().set(key, toStoreValue(value));
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public <T> T get(String key, Callable<T> valueLoader) {
-        return null;
+        Assert.hasText(key, "Key is null", 400);
+        Assert.notNull(valueLoader, "Value loader is null", 400);
+
+        Boolean hasKey = redisTemplate.hasKey(key);
+        boolean existKey = hasKey != null && hasKey;
+        if (existKey) {
+            return (T) fromStoreValue(redisTemplate.opsForValue().get(key));
+        }
+        try {
+            redisTemplate.opsForValue().set(key, toStoreValue(valueLoader.call()));
+        } catch (Exception e) {
+            throw new HotCloudException(String.format("Value for key '%s' could not be loaded using '%s'", key, valueLoader));
+        }
+
+        return (T) get(key);
     }
 
     @Override
     public void evict(String key) {
-
+        Assert.hasText(key, "Key is null", 400);
+        redisTemplate.delete(key);
+        log.info("Evict key '{}'", key);
     }
 
     @Override
     public void clear() {
-
+        Set<String> keys = redisTemplate.keys("*");
+        if (CollectionUtils.isEmpty(keys)) {
+            return;
+        }
+        redisTemplate.delete(keys);
+        log.info("Clear all keys");
     }
 }
