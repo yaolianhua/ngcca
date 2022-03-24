436df2b99e4506099ec0758faefb53804c6bd176
Jason <yaolianhua789@gmail.com>
2022-03-24 19:15:57 +0800

    [kubernetes-model] optimize

--- hotcloud-kubernetes/hotcloud-kubernetes-model/src/main/java/io/hotcloud/kubernetes/model/storage/HostPathVolume.java
+++ hotcloud-kubernetes/hotcloud-kubernetes-model/src/main/java/io/hotcloud/kubernetes/model/storage/HostPathVolume.java
@@ -10,4 +10,11 @@ public class HostPathVolume {
 
     private String path;
     private String type;
+
+    public static HostPathVolume of(String path, String type) {
+        HostPathVolume hostPathVolume = new HostPathVolume();
+        hostPathVolume.setPath(path);
+        hostPathVolume.setType(type);
+        return hostPathVolume;
+    }
 }
