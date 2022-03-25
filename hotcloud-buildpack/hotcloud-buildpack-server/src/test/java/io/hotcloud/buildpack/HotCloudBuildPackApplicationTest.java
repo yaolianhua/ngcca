4c5c0fb1c0313885a590bb57d76e3242ee808700
Jason <yaolianhua789@gmail.com>
2022-03-25 20:58:27 +0800

    [buildpack] IT

--- hotcloud-buildpack/hotcloud-buildpack-server/src/test/java/io/hotcloud/buildpack/HotCloudBuildPackApplicationTest.java
+++ hotcloud-buildpack/hotcloud-buildpack-server/src/test/java/io/hotcloud/buildpack/HotCloudBuildPackApplicationTest.java
@@ -1,11 +1,21 @@
 package io.hotcloud.buildpack;
 
+import io.hotcloud.common.spring.AsyncConfiguration;
+import io.hotcloud.common.spring.CacheConfiguration;
+import io.hotcloud.kubernetes.server.KubernetesApiConfiguration;
+import io.hotcloud.message.server.MessageConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author yaolianhua789@gmail.com
  **/
-@SpringBootApplication(scanBasePackageClasses = HotCloudBuildPackApplicationTest.class)
+@SpringBootApplication(scanBasePackageClasses = {
+        HotCloudBuildPackApplicationTest.class,
+        KubernetesApiConfiguration.class,
+        AsyncConfiguration.class,
+        MessageConfiguration.class,
+        CacheConfiguration.class
+})
 public class HotCloudBuildPackApplicationTest {
     //
 }
