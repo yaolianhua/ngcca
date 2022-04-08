f8a294166b7d7f658af018d0f126ae78a0cc77dc
Jason <yaolianhua789@gmail.com>
2022-04-08 09:53:34 +0800

    [security] optimize SecurityApplicationInternalUserPostProcessor

--- hotcloud-security/hotcloud-security-admin/src/main/java/io/hotcloud/security/admin/user/SecurityApplicationInternalUserPostProcessor.java
+++ hotcloud-security/hotcloud-security-admin/src/main/java/io/hotcloud/security/admin/user/SecurityApplicationInternalUserPostProcessor.java
@@ -4,9 +4,10 @@ import io.hotcloud.security.SecurityApplicationRunnerPostProcessor;
 import io.hotcloud.security.api.UserApi;
 import io.hotcloud.security.user.model.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Stream;
 
 /**
  * @author yaolianhua789@gmail.com
@@ -24,42 +25,20 @@ public class SecurityApplicationInternalUserPostProcessor implements SecurityApp
         this.passwordEncoder = passwordEncoder;
     }
 
+    private void internalUserSaved(String username) {
+        if (!userApi.exist(username)) {
+            User user = User.builder()
+                    .username(username)
+                    .password(passwordEncoder.encode("e2c20178-1f6b-4860-b9d2-7ac4a9f2a2ea"))
+                    .nickname(username)
+                    .build();
+            User saved = userApi.save(user);
+            log.info("SecurityApplicationInternalUserPostProcessor. {} user created", saved.getUsername());
+        }
+    }
+
     @Override
     public void execute() {
-        boolean adminExisted = userApi.exist("admin");
-        boolean guestExisted = userApi.exist("guest");
-        boolean clientUserExisted = userApi.exist("clientuser");
-
-        if (!adminExisted) {
-            User adminUser = User.builder()
-                    .username("admin")
-                    .password(passwordEncoder.encode("e2c20178-1f6b-4860-b9d2-7ac4a9f2a2ea"))
-                    .nickname("administrator")
-                    .enabled(true)
-                    .build();
-            UserDetails saved = userApi.save(adminUser);
-            log.info("SecurityApplicationInternalUserPostProcessor. {} user created", saved.getUsername());
-        }
-        if (!clientUserExisted) {
-            User clientUser = User.builder()
-                    .username("clientuser")
-                    .password(passwordEncoder.encode("e2c20178-1f6b-4860-b9d2-7ac4a9f2a2ea"))
-                    .nickname("clientuser")
-                    .enabled(true)
-                    .build();
-            UserDetails saved = userApi.save(clientUser);
-            log.info("SecurityApplicationInternalUserPostProcessor. {} user created", saved.getUsername());
-        }
-        if (!guestExisted) {
-            User guest = User.builder()
-                    .username("guest")
-                    .password(passwordEncoder.encode("e2c20178-1f6b-4860-b9d2-7ac4a9f2a2ea"))
-                    .nickname("guest")
-                    .enabled(true)
-                    .build();
-            UserDetails saved = userApi.save(guest);
-            log.info("SecurityApplicationInternalUserPostProcessor. {} user created", saved.getUsername());
-        }
-
+        Stream.of("admin", "guest", "clientuser").forEach(this::internalUserSaved);
     }
 }
