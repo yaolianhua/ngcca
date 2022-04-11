43a4679bbfe4ef122bf6c0210d852787dcfdf3b1
Jason <yaolianhua789@gmail.com>
2022-04-11 14:56:08 +0800

    [buildpack] BuildPackListener

--- hotcloud-buildpack/hotcloud-buildpack-server/src/main/java/io/hotcloud/buildpack/server/core/BuildPackListener.java
+++ hotcloud-buildpack/hotcloud-buildpack-server/src/main/java/io/hotcloud/buildpack/server/core/BuildPackListener.java
@@ -1,11 +1,16 @@
 package io.hotcloud.buildpack.server.core;
 
+import io.hotcloud.buildpack.api.core.BuildPackService;
 import io.hotcloud.buildpack.api.core.event.BuildPackStartFailureEvent;
 import io.hotcloud.buildpack.api.core.event.BuildPackStartedEvent;
+import io.hotcloud.buildpack.api.core.model.BuildPack;
+import io.hotcloud.buildpack.api.core.model.DefaultBuildPack;
+import io.hotcloud.common.Assert;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 /**
  * @author yaolianhua789@gmail.com
@@ -13,6 +18,12 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class BuildPackListener {
+
+    private final BuildPackService buildPackService;
+
+    public BuildPackListener(BuildPackService buildPackService) {
+        this.buildPackService = buildPackService;
+    }
 
     @Async
     @EventListener
@@ -23,6 +34,19 @@ public class BuildPackListener {
     @Async
     @EventListener
     public void startFailure(BuildPackStartFailureEvent startFailureEvent) {
-        log.info("BuildPackListener. {}", startFailureEvent);
+        try {
+            DefaultBuildPack buildPack = (DefaultBuildPack) startFailureEvent.getBuildPack();
+            Throwable throwable = startFailureEvent.getThrowable();
+            String message = StringUtils.hasText(throwable.getMessage()) ? throwable.getMessage() : throwable.getCause().getMessage();
+
+            buildPack.setDone(true);
+            buildPack.setMessage(message);
+
+            Assert.hasText(buildPack.getId(), "BuildPack ID is null");
+            BuildPack saveOrUpdate = buildPackService.saveOrUpdate(buildPack);
+            log.info("[BuildPackStartFailureEvent]. save or update buildPack [{}]", saveOrUpdate.getId());
+        } catch (Throwable e) {
+            log.error("[BuildPackStartFailureEvent] error. {}", e.getMessage(), e);
+        }
     }
 }
