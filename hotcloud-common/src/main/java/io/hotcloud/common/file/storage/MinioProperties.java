293c509b72a9d8a5f103af9846265b2085d26ee8
Jason <yaolianhua789@gmail.com>
2022-04-14 17:03:41 +0800

    [common] add MinioConfiguration

---
+++ hotcloud-common/src/main/java/io/hotcloud/common/file/storage/MinioProperties.java
@@ -0,0 +1,35 @@
+package io.hotcloud.common.file.storage;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author yaolianhua789@gmail.com
+ **/
+@Data
+@ConfigurationProperties(prefix = "minio")
+@Slf4j
+public class MinioProperties {
+
+    /**
+     * Access key for minio
+     */
+    private String accessKey;
+
+    /**
+     * Secret key for minio
+     */
+    private String secretKey;
+
+    /**
+     * Minio endpoint url
+     */
+    private String endpoint;
+
+    /**
+     * Minio default bucket
+     */
+    private String defaultBucket = "default";
+
+}
