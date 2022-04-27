1a6e465335196ead9a15dd2321f8f78db4d77dbc
Jason <yaolianhua789@gmail.com>
2022-04-27 15:23:58 +0800

    [buildpack] add BuildPackCollectionQuery

--- hotcloud-buildpack/hotcloud-buildpack-server/src/main/java/io/hotcloud/buildpack/server/controller/BuildPackController.java
+++ hotcloud-buildpack/hotcloud-buildpack-server/src/main/java/io/hotcloud/buildpack/server/controller/BuildPackController.java
@@ -2,24 +2,29 @@ package io.hotcloud.buildpack.server.controller;
 
 import io.hotcloud.buildpack.api.core.BuildPackPlayer;
 import io.hotcloud.buildpack.api.core.model.BuildPack;
+import io.hotcloud.buildpack.server.core.BuildPackCollectionQuery;
+import io.hotcloud.common.PageResult;
+import io.hotcloud.common.Pageable;
 import io.hotcloud.common.Result;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static io.hotcloud.common.WebResponse.accepted;
-import static io.hotcloud.common.WebResponse.created;
+import static io.hotcloud.common.WebResponse.*;
 
 /**
  * @author yaolianhua789@gmail.com
  **/
 @RestController
-@RequestMapping("/v1/buildpack")
+@RequestMapping("/v1/buildpacks")
 public class BuildPackController {
 
     private final BuildPackPlayer buildPackPlayer;
+    private final BuildPackCollectionQuery buildPackCollectionQuery;
 
-    public BuildPackController(BuildPackPlayer buildPackPlayer) {
+    public BuildPackController(BuildPackPlayer buildPackPlayer,
+                               BuildPackCollectionQuery buildPackCollectionQuery) {
         this.buildPackPlayer = buildPackPlayer;
+        this.buildPackCollectionQuery = buildPackCollectionQuery;
     }
 
     @PostMapping
@@ -35,6 +40,18 @@ public class BuildPackController {
     public ResponseEntity<Result<Void>> delete(@PathVariable("id") String id) {
         buildPackPlayer.delete(id);
         return accepted();
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResult<BuildPack>> find(@RequestParam(value = "user", required = false) String user,
+                                                      @RequestParam(value = "cloned_id", required = false) String clonedId,
+                                                      @RequestParam(value = "done", required = false) Boolean done,
+                                                      @RequestParam(value = "deleted", required = false) Boolean deleted,
+                                                      @RequestParam(value = "page", required = false) Integer page,
+                                                      @RequestParam(value = "page_size", required = false) Integer pageSize) {
+
+        PageResult<BuildPack> pageResult = buildPackCollectionQuery.pagingQuery(user, clonedId, done, deleted, Pageable.of(page, pageSize));
+        return okPage(pageResult);
     }
 
 }
