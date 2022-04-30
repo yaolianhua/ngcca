2c2f79e73ed3f0de07bc23f550c0268e8d88fdd5
Jason <yaolianhua789@gmail.com>
2022-04-30 18:23:12 +0800

    [application] optimize InstanceTemplateConstant

--- hotcloud-application/hotcloud-application-api/src/main/java/io/hotcloud/application/api/template/InstanceTemplateConstant.java
+++ hotcloud-application/hotcloud-application-api/src/main/java/io/hotcloud/application/api/template/InstanceTemplateConstant.java
@@ -4,8 +4,6 @@ package io.hotcloud.application.api.template;
  * @author yaolianhua789@gmail.com
  **/
 public class InstanceTemplateConstant {
-
-    public static final String MONGO = "mongo";
     public static final Integer MONGO_NODEPORT = 30117;
     public static final String MONGO_IMAGE = "mongo:5.0";
 
