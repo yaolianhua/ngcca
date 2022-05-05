dfea24ee723c0e8514feff0768fff2ec074a7901
Jason <yaolianhua789@gmail.com>
2022-05-05 11:14:40 +0800

    [application] optimize InstanceTemplateResourceHolder

--- hotcloud-application/hotcloud-application-api/src/main/java/io/hotcloud/application/api/template/InstanceTemplateResourceHolder.java
+++ hotcloud-application/hotcloud-application-api/src/main/java/io/hotcloud/application/api/template/InstanceTemplateResourceHolder.java
@@ -1,5 +1,7 @@
 package io.hotcloud.application.api.template;
 
+import org.springframework.util.Assert;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -11,10 +13,13 @@ public class InstanceTemplateResourceHolder {
     private final Map<Template, String> holder = new ConcurrentHashMap<>(32);
 
     public void put(Template type, String template) {
+        Assert.hasText(template, "Instance template is null");
+        Assert.notNull(type, "Instance template type is null");
         holder.put(type, template);
     }
 
     public String get(Template type) {
+        Assert.notNull(type, "Instance template type is null");
         return holder.get(type);
     }
 }
