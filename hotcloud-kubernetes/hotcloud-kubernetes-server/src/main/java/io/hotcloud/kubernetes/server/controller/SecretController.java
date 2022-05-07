5e6676e8171673f823cf7c78e1e310ded8adff93
Jason <yaolianhua789@gmail.com>
2022-05-07 16:22:18 +0800

    [kubernetes] optimize springdoc

--- hotcloud-kubernetes/hotcloud-kubernetes-server/src/main/java/io/hotcloud/kubernetes/server/controller/SecretController.java
+++ hotcloud-kubernetes/hotcloud-kubernetes-server/src/main/java/io/hotcloud/kubernetes/server/controller/SecretController.java
@@ -8,6 +8,10 @@ import io.hotcloud.kubernetes.api.configurations.SecretApi;
 import io.hotcloud.kubernetes.model.SecretCreateRequest;
 import io.hotcloud.kubernetes.model.YamlBody;
 import io.kubernetes.client.openapi.ApiException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +23,7 @@ import java.util.Map;
  **/
 @RestController
 @RequestMapping("/v1/kubernetes/secrets")
+@Tag(name = "Kubernetes Secret")
 public class SecretController {
 
     private final SecretApi secretApi;
@@ -28,6 +33,11 @@ public class SecretController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "Secret create with request body",
+            responses = {@ApiResponse(responseCode = "201")},
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Secret request body")
+    )
     public ResponseEntity<Result<Secret>> secret(@RequestBody SecretCreateRequest params) throws ApiException {
         Secret secret = secretApi.secret(params);
 
@@ -35,12 +45,25 @@ public class SecretController {
     }
 
     @PostMapping("/yaml")
+    @Operation(
+            summary = "Secret create with kubernetes yaml",
+            responses = {@ApiResponse(responseCode = "201")},
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Secret kubernetes yaml")
+    )
     public ResponseEntity<Result<Secret>> secret(@RequestBody YamlBody yaml) throws ApiException {
         Secret secret = secretApi.secret(yaml.getYaml());
         return WebResponse.created(secret);
     }
 
     @GetMapping("/{namespace}/{secret}")
+    @Operation(
+            summary = "Secret read",
+            responses = {@ApiResponse(responseCode = "200")},
+            parameters = {
+                    @Parameter(name = "namespace", description = "kubernetes namespace"),
+                    @Parameter(name = "secret", description = "secret name")
+            }
+    )
     public ResponseEntity<Result<Secret>> secretRead(@PathVariable String namespace,
                                                      @PathVariable String secret) {
         Secret read = secretApi.read(namespace, secret);
@@ -48,6 +71,13 @@ public class SecretController {
     }
 
     @GetMapping("/{namespace}")
+    @Operation(
+            summary = "Secret collection read",
+            responses = {@ApiResponse(responseCode = "200")},
+            parameters = {
+                    @Parameter(name = "namespace", description = "kubernetes namespace")
+            }
+    )
     public ResponseEntity<Result<SecretList>> secretListRead(@PathVariable String namespace,
                                                              @RequestParam(required = false) Map<String, String> labelSelector) {
         SecretList list = secretApi.read(namespace, labelSelector);
@@ -55,6 +85,14 @@ public class SecretController {
     }
 
     @DeleteMapping("/{namespace}/{secret}")
+    @Operation(
+            summary = "Secret delete",
+            responses = {@ApiResponse(responseCode = "202")},
+            parameters = {
+                    @Parameter(name = "namespace", description = "kubernetes namespace"),
+                    @Parameter(name = "secret", description = "secret name")
+            }
+    )
     public ResponseEntity<Result<Void>> secretDelete(@PathVariable String namespace,
                                                      @PathVariable String secret) throws ApiException {
         secretApi.delete(namespace, secret);
