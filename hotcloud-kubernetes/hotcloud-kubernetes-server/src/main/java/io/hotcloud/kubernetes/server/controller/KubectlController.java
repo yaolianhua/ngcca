04e6b882b5cccf6153c7dd963c513e019beb3e6e
Jason <yaolianhua789@gmail.com>
2022-05-07 17:15:12 +0800

    [kubernetes] optimize springdoc

--- hotcloud-kubernetes/hotcloud-kubernetes-server/src/main/java/io/hotcloud/kubernetes/server/controller/KubectlController.java
+++ hotcloud-kubernetes/hotcloud-kubernetes-server/src/main/java/io/hotcloud/kubernetes/server/controller/KubectlController.java
@@ -7,6 +7,11 @@ import io.hotcloud.common.WebResponse;
 import io.hotcloud.kubernetes.api.equianlent.CopyAction;
 import io.hotcloud.kubernetes.api.equianlent.KubectlApi;
 import io.hotcloud.kubernetes.model.YamlBody;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +24,7 @@ import java.util.concurrent.TimeUnit;
  **/
 @RestController
 @RequestMapping("/v1/kubernetes/equivalents")
+@Tag(name = "Kubernetes equivalents")
 public class KubectlController {
 
     private final KubectlApi kubectlApi;
@@ -28,6 +34,14 @@ public class KubectlController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "kubectl apply ",
+            responses = {@ApiResponse(responseCode = "201")},
+            parameters = {
+                    @Parameter(name = "namespace", description = "kubernetes namespace")
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "kubernetes yaml body")
+    )
     public ResponseEntity<Result<List<HasMetadata>>> resourceListCreateOrReplace(@RequestParam(value = "namespace",required = false) String namespace,
                                                          @RequestBody YamlBody yaml) {
         List<HasMetadata> hasMetadata = kubectlApi.apply(namespace, yaml.getYaml());
@@ -35,6 +49,14 @@ public class KubectlController {
     }
 
     @DeleteMapping
+    @Operation(
+            summary = "kubectl delete ",
+            responses = {@ApiResponse(responseCode = "202")},
+            parameters = {
+                    @Parameter(name = "namespace", description = "kubernetes namespace")
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "kubernetes yaml body")
+    )
     public ResponseEntity<Result<Boolean>> resourceListDelete(@RequestParam(value = "namespace", required = false) String namespace,
                                                               @RequestBody YamlBody yaml) {
         Boolean delete = kubectlApi.delete(namespace, yaml.getYaml());
@@ -42,6 +64,20 @@ public class KubectlController {
     }
 
     @PostMapping("/{namespace}/{pod}/forward")
+    @Operation(
+            summary = "Listen on port localPort on selected IP inetAddress, forwarding to port in the pod",
+            description = "kubectl port-forward --address {@code ipv4} pod/pod-name 8888:5000",
+            responses = {@ApiResponse(responseCode = "202")},
+            parameters = {
+                    @Parameter(name = "namespace", description = "kubernetes namespace"),
+                    @Parameter(name = "pod", description = "pod name"),
+                    @Parameter(name = "ipv4Address", description = "listened address", schema = @Schema(defaultValue = "127.0.0.1")),
+                    @Parameter(name = "containerPort", description = "container port"),
+                    @Parameter(name = "localPort", description = "listened local port"),
+                    @Parameter(name = "alive", description = "alive times", schema = @Schema(defaultValue = "10L")),
+                    @Parameter(name = "timeUnit", description = "alive time unit", schema = @Schema(defaultValue = "TimeUnit.MINUTES"))
+            }
+    )
     public ResponseEntity<Result<Boolean>> portForward(@PathVariable(value = "namespace") String namespace,
                                                        @PathVariable(value = "pod") String pod,
                                                        @RequestParam(value = "ipv4Address", required = false) String address,
@@ -54,6 +90,19 @@ public class KubectlController {
     }
 
     @PostMapping("/{namespace}/{pod}/upload")
+    @Operation(
+            summary = "Upload local file/dir to inside Pod",
+            description = "kubectl cp /tmp/foo some-pod:/tmp/bar -c specific-container",
+            responses = {@ApiResponse(responseCode = "202")},
+            parameters = {
+                    @Parameter(name = "namespace", description = "kubernetes namespace"),
+                    @Parameter(name = "pod", description = "pod name"),
+                    @Parameter(name = "container", description = "container name in Pod. can be null if only one container in Pod"),
+                    @Parameter(name = "source", description = "local file/dir path"),
+                    @Parameter(name = "target", description = "remote pod file/dir path"),
+                    @Parameter(name = "action", description = "copy action enums", schema = @Schema(allowableValues = {"FILE", "DIRECTORY"}))
+            }
+    )
     public ResponseEntity<Result<Boolean>> upload(@PathVariable(value = "namespace") String namespace,
                                                   @PathVariable(value = "pod") String pod,
                                                   @RequestParam(value = "container", required = false) String container,
@@ -65,6 +114,19 @@ public class KubectlController {
     }
 
     @PostMapping("/{namespace}/{pod}/download")
+    @Operation(
+            summary = "Download remote Pod file/dir to locally",
+            description = "kubectl cp some-namespace/some-pod:/tmp/foo /tmp/bar",
+            responses = {@ApiResponse(responseCode = "202")},
+            parameters = {
+                    @Parameter(name = "namespace", description = "kubernetes namespace"),
+                    @Parameter(name = "pod", description = "pod name"),
+                    @Parameter(name = "container", description = "container name in Pod. can be null if only one container in Pod"),
+                    @Parameter(name = "source", description = "remote pod file/dir path"),
+                    @Parameter(name = "target", description = "local file/dir path"),
+                    @Parameter(name = "action", description = "copy action enums", schema = @Schema(allowableValues = {"FILE", "DIRECTORY"}))
+            }
+    )
     public ResponseEntity<Result<Boolean>> download(@PathVariable(value = "namespace") String namespace,
                                                     @PathVariable(value = "pod") String pod,
                                                     @RequestParam(value = "container", required = false) String container,
@@ -76,13 +138,28 @@ public class KubectlController {
     }
 
     @GetMapping("/{namespace}/events")
+    @Operation(
+            summary = "Events collection read",
+            responses = {@ApiResponse(responseCode = "200")},
+            parameters = {
+                    @Parameter(name = "namespace", description = "kubernetes namespace")
+            }
+    )
     public ResponseEntity<Result<List<Event>>> events(@PathVariable(value = "namespace") String namespace) {
         return WebResponse.ok(kubectlApi.events(namespace));
     }
 
-    @GetMapping("/{namespace}/events/{name}")
+    @GetMapping("/{namespace}/events/{event}")
+    @Operation(
+            summary = "Events read",
+            responses = {@ApiResponse(responseCode = "200")},
+            parameters = {
+                    @Parameter(name = "namespace", description = "kubernetes namespace"),
+                    @Parameter(name = "event", description = "event name")
+            }
+    )
     public ResponseEntity<Result<Event>> events(@PathVariable(value = "namespace") String namespace,
-                                                @PathVariable(value = "name") String name) {
+                                                @PathVariable(value = "event") String name) {
         return WebResponse.ok(kubectlApi.events(namespace, name));
     }
 
