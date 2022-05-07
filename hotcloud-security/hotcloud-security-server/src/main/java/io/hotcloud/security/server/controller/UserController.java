864aa799363440dd5d56841eaa33fa3de49c2035
Jason <yaolianhua789@gmail.com>
2022-05-07 10:59:35 +0800

    [security] optimize springdoc

--- hotcloud-security/hotcloud-security-server/src/main/java/io/hotcloud/security/server/controller/UserController.java
+++ hotcloud-security/hotcloud-security-server/src/main/java/io/hotcloud/security/server/controller/UserController.java
@@ -7,6 +7,11 @@ import io.hotcloud.common.WebResponse;
 import io.hotcloud.security.api.user.User;
 import io.hotcloud.security.api.user.UserApi;
 import io.hotcloud.security.server.user.UserCollectionQuery;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
  **/
 @RestController
 @RequestMapping("/v1/security/users")
+@Tag(name = "Security user")
 public class UserController {
 
     private final UserApi userApi;
@@ -27,12 +33,25 @@ public class UserController {
     }
 
     @PutMapping
+    @Operation(
+            summary = "User object update",
+            responses = {@ApiResponse(responseCode = "202")},
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "User object, id can not be null!")
+    )
     public ResponseEntity<Result<User>> update(@RequestBody User user) {
         User updated = userApi.update(user);
         return WebResponse.accepted(updated);
     }
 
     @PutMapping("/{username}/{enable}")
+    @Operation(
+            summary = "User status update",
+            responses = {@ApiResponse(responseCode = "202")},
+            parameters = {
+                    @Parameter(name = "username", description = "username"),
+                    @Parameter(name = "enable", description = "The giving username will be disable if enable=false", schema = @Schema(allowableValues = {"true", "false"}))
+            }
+    )
     public ResponseEntity<Result<Void>> onOff(@PathVariable("username") String username,
                                               @PathVariable("enable") Boolean enable) {
         userApi.switchUser(username, enable);
@@ -40,24 +59,53 @@ public class UserController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "User object save",
+            responses = {@ApiResponse(responseCode = "201")},
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "User object, id will be auto generate")
+    )
     public ResponseEntity<Result<User>> save(@RequestBody User user) {
         User saved = userApi.save(user);
         return WebResponse.created(saved);
     }
 
     @DeleteMapping("/{username}")
+    @Operation(
+            summary = "User delete, it will be deleted physically",
+            responses = {@ApiResponse(responseCode = "202")},
+            parameters = {
+                    @Parameter(name = "username", description = "which user will delete")
+            }
+    )
     public ResponseEntity<Result<Boolean>> delete(@PathVariable("username") String username) {
         boolean deleted = userApi.delete(username, true);
         return WebResponse.accepted(deleted);
     }
 
     @GetMapping("/{username}")
+    @Operation(
+            summary = "User query",
+            responses = {@ApiResponse(responseCode = "200")},
+            parameters = {
+                    @Parameter(name = "username", description = "username queried")
+            }
+    )
     public ResponseEntity<Result<User>> user(@PathVariable("username") String username) {
         User user = userApi.retrieve(username);
         return WebResponse.ok(user);
     }
 
     @GetMapping
+    @Operation(
+            summary = "User paging query",
+            responses = {@ApiResponse(responseCode = "200")},
+            parameters = {
+                    @Parameter(name = "username", description = "username queried"),
+                    @Parameter(name = "enabled", description = "user's status", schema = @Schema(allowableValues = {"true", "false"})),
+                    @Parameter(name = "page", description = "current page", schema = @Schema(defaultValue = "1")),
+                    @Parameter(name = "page_size", description = "pageSize", schema = @Schema(defaultValue = "10"))
+            }
+    )
     public ResponseEntity<PageResult<User>> page(@RequestParam(value = "username", required = false) String username,
                                                  @RequestParam(value = "enabled", required = false) Boolean enabled,
                                                  @RequestParam(value = "page", required = false) Integer page,
