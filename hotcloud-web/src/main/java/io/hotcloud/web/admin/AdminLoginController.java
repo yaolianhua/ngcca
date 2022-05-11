52ebdc1ba2f186165c12c7a08ccbf1c7e44d59d6
Jason <yaolianhua789@gmail.com>
2022-05-11 10:43:14 +0800

    [web] refactor login

--- hotcloud-web/src/main/java/io/hotcloud/web/admin/AdminLoginController.java
+++ hotcloud-web/src/main/java/io/hotcloud/web/admin/AdminLoginController.java
@@ -1,14 +1,18 @@
-package io.hotcloud.web.login;
+package io.hotcloud.web.admin;
 
 import io.hotcloud.security.api.login.BearerToken;
 import io.hotcloud.security.api.user.User;
 import io.hotcloud.web.ClientAuthorizationManager;
 import io.hotcloud.web.R;
+import io.hotcloud.web.login.LoginClient;
 import io.hotcloud.web.user.UserClient;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,25 +21,33 @@ import java.util.Objects;
 /**
  * @author yaolianhua789@gmail.com
  **/
-@Component
-@Slf4j
-public class LoginService {
+@Controller
+@RequestMapping("/administrator/login")
+public class AdminLoginController {
+
     private final LoginClient loginClient;
     private final UserClient userClient;
     private final ClientAuthorizationManager authorizationManager;
 
-    public LoginService(LoginClient loginClient,
-                        UserClient userClient,
-                        ClientAuthorizationManager authorizationManager) {
+    public AdminLoginController(LoginClient loginClient,
+                                UserClient userClient,
+                                ClientAuthorizationManager authorizationManager) {
         this.loginClient = loginClient;
         this.userClient = userClient;
         this.authorizationManager = authorizationManager;
     }
 
-    public String login(Model model, RedirectAttributes redirect, HttpServletRequest request,
-                        String username,
-                        String password,
-                        boolean adminLogin) {
+    @GetMapping
+    public String adminLoginPage() {
+        return "admin/login";
+    }
+
+    @PostMapping
+    public String adminLogin(Model model,
+                             RedirectAttributes redirect,
+                             HttpServletRequest request,
+                             @ModelAttribute("username") String username,
+                             @ModelAttribute("password") String password) {
         ResponseEntity<R<BearerToken>> entity = loginClient.login(username, password);
         boolean successful = entity.getStatusCode().is2xxSuccessful();
         if (successful) {
@@ -46,22 +58,20 @@ public class LoginService {
             redirect.addFlashAttribute("user", Objects.requireNonNull(body).getData());
             redirect.addFlashAttribute("authorization", Objects.requireNonNull(entity.getBody()).getData().getAuthorization());
 
-            if (adminLogin) {
-                if (!isAdmin(username)) {
-                    model.addAttribute("message", "non-admin account");
-                    return "admin/login";
-                } else {
-                    return "redirect:/administrator/index";
-                }
+            if (!isAdmin(username)) {
+                model.addAttribute("message", "non-admin account");
+                return "admin/login";
+            } else {
+                return "redirect:/administrator/index";
             }
-            return "redirect:/index";
         }
 
         model.addAttribute("message", Objects.requireNonNull(entity.getBody()).getMessage());
-        return adminLogin ? "admin/login" : "login";
+        return "admin/login";
     }
 
     private boolean isAdmin(String username) {
         return Objects.equals("admin", username);
     }
+
 }
--- hotcloud-web/src/main/java/io/hotcloud/web/login/LoginController.java
+++ hotcloud-web/src/main/java/io/hotcloud/web/login/LoginController.java
@@ -1,5 +1,11 @@
 package io.hotcloud.web.login;
 
+import io.hotcloud.security.api.login.BearerToken;
+import io.hotcloud.security.api.user.User;
+import io.hotcloud.web.ClientAuthorizationManager;
+import io.hotcloud.web.R;
+import io.hotcloud.web.user.UserClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
 /**
  * @author yaolianhua789@gmail.com
@@ -17,20 +24,21 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping
 public class LoginController {
 
-    private final LoginService loginService;
+    private final LoginClient loginClient;
+    private final UserClient userClient;
+    private final ClientAuthorizationManager authorizationManager;
 
-    public LoginController(LoginService loginService) {
-        this.loginService = loginService;
+    public LoginController(LoginClient loginClient,
+                           UserClient userClient,
+                           ClientAuthorizationManager authorizationManager) {
+        this.loginClient = loginClient;
+        this.userClient = userClient;
+        this.authorizationManager = authorizationManager;
     }
 
     @GetMapping("/login")
     public String loginPage() {
         return "login";
-    }
-
-    @GetMapping("/administrator/login")
-    public String adminLoginPage() {
-        return "admin/login";
     }
 
     @PostMapping("/login")
@@ -39,16 +47,20 @@ public class LoginController {
                         HttpServletRequest request,
                         @ModelAttribute("username") String username,
                         @ModelAttribute("password") String password) {
-        return loginService.login(model, redirect, request, username, password, false);
-    }
+        ResponseEntity<R<BearerToken>> entity = loginClient.login(username, password);
+        boolean successful = entity.getStatusCode().is2xxSuccessful();
+        if (successful) {
+            BearerToken bearerToken = Objects.requireNonNull(entity.getBody()).getData();
+            authorizationManager.add(request.getSession().getId(), bearerToken.getAuthorization());
 
-    @PostMapping("/administrator/login")
-    public String adminLogin(Model model,
-                             RedirectAttributes redirect,
-                             HttpServletRequest request,
-                             @ModelAttribute("username") String username,
-                             @ModelAttribute("password") String password) {
-        return loginService.login(model, redirect, request, username, password, true);
+            R<User> body = userClient.user(username).getBody();
+            redirect.addFlashAttribute("user", Objects.requireNonNull(body).getData());
+            redirect.addFlashAttribute("authorization", Objects.requireNonNull(entity.getBody()).getData().getAuthorization());
+            return "redirect:/index";
+        }
+
+        model.addAttribute("message", Objects.requireNonNull(entity.getBody()).getMessage());
+        return "login";
     }
 
 }
