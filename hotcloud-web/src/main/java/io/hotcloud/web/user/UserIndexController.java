11100e5287a40fb22ee5c69182fb2fd81a21361e
Jason <yaolianhua789@gmail.com>
2022-05-12 18:02:41 +0800

    [web] update

--- hotcloud-web/src/main/java/io/hotcloud/web/user/UserIndexController.java
+++ hotcloud-web/src/main/java/io/hotcloud/web/user/UserIndexController.java
@@ -1,5 +1,6 @@
-package io.hotcloud.web;
+package io.hotcloud.web.user;
 
+import io.hotcloud.web.WebUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  **/
 @Controller
 @RequestMapping
-public class IndexController {
+public class UserIndexController {
     @RequestMapping(value = {"/index", "/"})
     @WebUser
     public String indexPage(Model model) {
