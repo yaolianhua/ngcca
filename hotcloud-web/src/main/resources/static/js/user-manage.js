6fee747aa0705fef8c9773a36c71d9136ffa20af
Jason <yaolianhua789@gmail.com>
2022-05-15 20:19:26 +0800

    [web] dropzone config

--- hotcloud-web/src/main/resources/static/js/user-manage.js
+++ hotcloud-web/src/main/resources/static/js/user-manage.js
@@ -93,7 +93,7 @@ function userEP(id) {
 //user detail view
 function userDP(id) {
     $('#users-fragment').load('/administrator/user-manage?action=detail&id=' + id, function () {
-
+        dropzone(id);
     });
 }
 
@@ -158,4 +158,79 @@ function userOff(user) {
         .catch(error => {
             fail(error);
         });
+}
+
+//Get authorization from cookies
+function getAuthorization() {
+    let strcookie = document.cookie;//获取cookie字符串
+    let arrcookie = strcookie.split("; ");//分割
+    //遍历匹配
+    for (let i = 0; i < arrcookie.length; i++) {
+        let arr = arrcookie[i].split("=");
+        if (arr[0] === "authorization") {
+            return arr[1];
+        }
+    }
+    return "";
+}
+
+function dropzone(id) {
+    // DropzoneJS Demo Code Start
+    Dropzone.autoDiscover = false
+
+    // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
+    let previewNode = document.querySelector("#template")
+    previewNode.id = ""
+    let previewTemplate = previewNode.parentNode.innerHTML
+    previewNode.parentNode.removeChild(previewNode)
+    let myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
+        url: "http://localhost:8080/v1/storage/upload?bucket=" + id, // Set the url
+        headers: {"Authorization": 'Bearer ' + getAuthorization()},
+        acceptedFiles: "image/jpg, image/png, image/gif",
+        success: function (file, response) {
+            alert(response.data);
+        },
+        thumbnailWidth: 80,
+        thumbnailHeight: 80,
+        parallelUploads: 20,
+        previewTemplate: previewTemplate,
+        autoQueue: false, // Make sure the files aren't queued until manually added
+        previewsContainer: "#previews", // Define the container to display the previews
+        clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+    })
+
+    myDropzone.on("addedfile", function (file) {
+        // Hookup the start button
+        file.previewElement.querySelector(".start").onclick = function () {
+            myDropzone.enqueueFile(file)
+        }
+    })
+
+    // Update the total progress bar
+    myDropzone.on("totaluploadprogress", function (progress) {
+        document.querySelector("#total-progress .progress-bar").style.width = progress + "%"
+    })
+
+    myDropzone.on("sending", function (file) {
+        // Show the total progress bar when upload starts
+        document.querySelector("#total-progress").style.opacity = "1"
+        // And disable the start button
+        file.previewElement.querySelector(".start").setAttribute("disabled", "disabled")
+    })
+
+    // Hide the total progress bar when nothing's uploading anymore
+    myDropzone.on("queuecomplete", function (progress) {
+        document.querySelector("#total-progress").style.opacity = "0"
+    })
+
+    // Setup the buttons for all transfers
+    // The "add files" button doesn't need to be setup because the config
+    // `clickable` has already been specified.
+    document.querySelector("#actions .start").onclick = function () {
+        myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED))
+    }
+    document.querySelector("#actions .cancel").onclick = function () {
+        myDropzone.removeAllFiles(true)
+    }
+    // DropzoneJS Demo Code End
 }
