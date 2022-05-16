2cccba33af1da178a3d0c8427df2bb3969bcbced
Jason <yaolianhua789@gmail.com>
2022-05-16 16:06:55 +0800

    [db] add ActivityEntity

---
+++ hotcloud-db/src/main/java/io/hotcloud/db/core/activity/ActivityEntity.java
@@ -0,0 +1,33 @@
+package io.hotcloud.db.core.activity;
+
+import io.hotcloud.db.core.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.redis.core.RedisHash;
+
+/**
+ * @author yaolianhua789@gmail.com
+ **/
+@Document("activities")
+@RedisHash("activities")
+@Getter
+@Setter
+public class ActivityEntity extends AbstractEntity {
+
+    @Indexed
+    @org.springframework.data.redis.core.index.Indexed
+    private String user;
+    private String namespace;
+    @Indexed
+    @org.springframework.data.redis.core.index.Indexed
+    private String target;
+    private String action;
+    private String description;
+    @Indexed
+    @org.springframework.data.redis.core.index.Indexed
+    private String targetId;
+    private String targetName;
+
+}
---
+++ hotcloud-db/src/main/java/io/hotcloud/db/core/activity/ActivityRepository.java
@@ -0,0 +1,9 @@
+package io.hotcloud.db.core.activity;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * @author yaolianhua789@gmail.com
+ **/
+public interface ActivityRepository extends PagingAndSortingRepository<ActivityEntity, String> {
+}
