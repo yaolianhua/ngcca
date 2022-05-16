604ad20a116801a4b1cc92f3c98844174086eddd
Jason <yaolianhua789@gmail.com>
2022-05-16 11:35:58 +0800

    [starter] rename package

--- hotcloud-allinone-serverside/src/main/java/io/hotcloud/allinone/HotCloudApplication.java
+++ hotcloud-allinone-serverside/src/main/java/io/hotcloud/allinone/HotCloudApplication.java
@@ -1,4 +1,4 @@
-package io.hotcloud.starter;
+package io.hotcloud.allinone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
