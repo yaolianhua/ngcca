6670049a664863a9ee2eaa2df87c669f6778f237
Jason <yaolianhua789@gmail.com>
2022-05-16 16:07:21 +0800

    [common] add ActivityLog

---
+++ hotcloud-common/src/main/java/io/hotcloud/common/activity/ActivityAction.java
@@ -0,0 +1,11 @@
+package io.hotcloud.common.activity;
+
+/**
+ * @author yaolianhua789@gmail.com
+ **/
+public enum ActivityAction {
+    //
+    Create,
+    Update,
+    Delete
+}
---
+++ hotcloud-common/src/main/java/io/hotcloud/common/activity/ActivityTarget.java
@@ -0,0 +1,12 @@
+package io.hotcloud.common.activity;
+
+/**
+ * @author yaolianhua789@gmail.com
+ **/
+public enum ActivityTarget {
+    //
+    Git_Clone,
+    BuildPack,
+    Instance_Template,
+    Application
+}
