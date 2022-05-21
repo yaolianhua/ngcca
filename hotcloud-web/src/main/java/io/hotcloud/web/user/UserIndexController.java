d7e82fbbbcd7b386c80781a7ffd6c9084a0f83d5
Jason <yaolianhua789@gmail.com>
2022-05-21 17:46:54 +0800

    [web] 更新活动demo

--- hotcloud-web/src/main/java/io/hotcloud/web/user/UserIndexController.java
+++ hotcloud-web/src/main/java/io/hotcloud/web/user/UserIndexController.java
@@ -1,5 +1,8 @@
 package io.hotcloud.web.user;
 
+import io.hotcloud.web.activity.Activity;
+import io.hotcloud.web.activity.ActivityClient;
+import io.hotcloud.web.mvc.PageResult;
 import io.hotcloud.web.mvc.Result;
 import io.hotcloud.web.mvc.WebConstant;
 import io.hotcloud.web.mvc.WebUser;
@@ -19,9 +22,12 @@ import java.util.Objects;
 public class UserIndexController {
 
     private final StatisticsClient statisticsClient;
+    private final ActivityClient activityClient;
 
-    public UserIndexController(StatisticsClient statisticsClient) {
+    public UserIndexController(StatisticsClient statisticsClient,
+                               ActivityClient activityClient) {
         this.statisticsClient = statisticsClient;
+        this.activityClient = activityClient;
     }
 
     @RequestMapping(value = {"/index", "/"})
@@ -29,7 +35,9 @@ public class UserIndexController {
     public String indexPage(Model model,
                             User user) {
         Result<Statistics> result = statisticsClient.statistics(user.getId()).getBody();
+        PageResult<Activity> pageResult = activityClient.activities(user.getUsername(), null, null, 1, 50).getBody();
         model.addAttribute(WebConstant.STATISTICS, Objects.requireNonNull(result).getData());
+        model.addAttribute(WebConstant.ACTIVITIES, Objects.requireNonNull(pageResult).getList());
         return "index";
     }
 }
