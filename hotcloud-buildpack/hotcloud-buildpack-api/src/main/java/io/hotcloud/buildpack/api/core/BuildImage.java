ea05e47d499d52b9a284f32d637b4dca426b7e37
Jason <yaolianhua789@gmail.com>
2022-07-28 20:44:15 +0800

    [buildpack] 优化 BuildImage

--- hotcloud-buildpack/hotcloud-buildpack-api/src/main/java/io/hotcloud/buildpack/api/core/BuildImage.java
+++ hotcloud-buildpack/hotcloud-buildpack-api/src/main/java/io/hotcloud/buildpack/api/core/BuildImage.java
@@ -1,6 +1,5 @@
 package io.hotcloud.buildpack.api.core;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -42,15 +41,14 @@ public class BuildImage {
                         .build()
         ).build();
     }
-    @JsonIgnore
     public boolean isSourceCode(){
         return source != null;
     }
-    @JsonIgnore
+
     public boolean isJar(){
         return jar != null;
     }
-    @JsonIgnore
+
     public boolean isWar(){
         return war != null;
     }
