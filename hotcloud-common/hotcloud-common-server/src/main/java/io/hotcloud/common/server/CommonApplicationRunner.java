55c68706598208bf8ed4c1afd294a29ed23d0958
yaolianhua <yaolianhua789@gmail.com>
2022-09-02 11:59:11 +0800

    [common] 调整runner优先级

--- hotcloud-common/hotcloud-common-server/src/main/java/io/hotcloud/common/server/CommonApplicationRunner.java
+++ hotcloud-common/hotcloud-common-server/src/main/java/io/hotcloud/common/server/CommonApplicationRunner.java
@@ -3,11 +3,14 @@ package io.hotcloud.common.server;
 import io.hotcloud.common.api.CommonRunnerProcessor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@Order(Ordered.LOWEST_PRECEDENCE - 3)
 public class CommonApplicationRunner implements ApplicationRunner {
 
     private final List<CommonRunnerProcessor> processors;
