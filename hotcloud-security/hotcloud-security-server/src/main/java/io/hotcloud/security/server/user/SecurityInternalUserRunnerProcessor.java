8dcb07896724baa6b3f424ec512283d1047ff71b
Jason <yaolianhua789@gmail.com>
2022-09-23 16:15:03 +0800

    [security] 优化 SecurityInternalUserRunnerProcessor

--- hotcloud-security/hotcloud-security-server/src/main/java/io/hotcloud/security/server/user/SecurityInternalUserRunnerProcessor.java
+++ hotcloud-security/hotcloud-security-server/src/main/java/io/hotcloud/security/server/user/SecurityInternalUserRunnerProcessor.java
@@ -1,11 +1,15 @@
 package io.hotcloud.security.server.user;
 
+import io.hotcloud.common.api.CommonConstant;
 import io.hotcloud.common.api.CommonRunnerProcessor;
 import io.hotcloud.common.api.Log;
+import io.hotcloud.common.api.UUIDGenerator;
+import io.hotcloud.common.api.cache.Cache;
 import io.hotcloud.security.api.user.User;
 import io.hotcloud.security.api.user.UserApi;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
 import java.util.stream.Stream;
 
 /**
@@ -15,9 +19,11 @@ import java.util.stream.Stream;
 public class SecurityInternalUserRunnerProcessor implements CommonRunnerProcessor {
 
     private final UserApi userApi;
+    private final Cache cache;
 
-    public SecurityInternalUserRunnerProcessor(UserApi userApi) {
+    public SecurityInternalUserRunnerProcessor(UserApi userApi, Cache cache) {
         this.userApi = userApi;
+        this.cache = cache;
     }
 
     private void internalUserSaved(String username) {
@@ -31,6 +37,14 @@ public class SecurityInternalUserRunnerProcessor implements CommonRunnerProcesso
             User saved = userApi.save(user);
             Log.info(SecurityInternalUserRunnerProcessor.class.getName(), String.format("%s user created", saved.getUsername()));
         }
+
+        String namespace = UUIDGenerator.uuidNoDash();
+        Object o = cache.putIfAbsent(String.format(CommonConstant.CK_NAMESPACE_USER_KEY_PREFIX, username), namespace);
+        if (Objects.isNull(o)){
+            Log.info(SecurityInternalUserRunnerProcessor.class.getName(),
+                    String.format("user '%s' namespace '%s' cached", username, namespace));
+        }
+
     }
 
     @Override
