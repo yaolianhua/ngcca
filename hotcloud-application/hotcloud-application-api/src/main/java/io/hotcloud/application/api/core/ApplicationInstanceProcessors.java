1a92d3398a64aeabc344227a898d7011b0ed2484
Jason <yaolianhua789@gmail.com>
2022-09-27 14:29:54 +0800

    [application] log

--- hotcloud-application/hotcloud-application-api/src/main/java/io/hotcloud/application/api/core/ApplicationInstanceProcessors.java
+++ hotcloud-application/hotcloud-application-api/src/main/java/io/hotcloud/application/api/core/ApplicationInstanceProcessors.java
@@ -37,7 +37,7 @@ public class ApplicationInstanceProcessors {
                         TimeUnit.SECONDS.sleep(1);
                         String status = cache.get(String.format(CommonConstant.CK_IMAGEBUILD_STATUS, instance.getBuildPackId()), String.class);
                         if (Objects.equals("Succeeded", status)){
-                            Log.error(ApplicationInstanceProcessors.class.getName(),
+                            Log.info(ApplicationInstanceProcessors.class.getName(),
                                     String.format("[%s] user's application instance [%s] image build pipeline succeed [%s]", instance.getUser(), instance.getName(), instance.getBuildPackId()));
                             break;
                         }
