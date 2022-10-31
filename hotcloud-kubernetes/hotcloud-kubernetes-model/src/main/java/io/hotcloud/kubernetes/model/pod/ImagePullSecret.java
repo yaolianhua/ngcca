3225c0e172a26c623fee346bcc39a194f1b91a5b
Jason <yaolianhua789@gmail.com>
2022-10-31 09:52:54 +0800

    [kubernetes] ImagePullSecret 构造器

--- hotcloud-kubernetes/hotcloud-kubernetes-model/src/main/java/io/hotcloud/kubernetes/model/pod/ImagePullSecret.java
+++ hotcloud-kubernetes/hotcloud-kubernetes-model/src/main/java/io/hotcloud/kubernetes/model/pod/ImagePullSecret.java
@@ -1,12 +1,12 @@
 package io.hotcloud.kubernetes.model.pod;
 
 import lombok.Data;
-
-/**
- * @author yaolianhua789@gmail.com
- **/
 @Data
 public class ImagePullSecret {
 
     private String name;
+
+    public ImagePullSecret(String name) {
+        this.name = name;
+    }
 }
