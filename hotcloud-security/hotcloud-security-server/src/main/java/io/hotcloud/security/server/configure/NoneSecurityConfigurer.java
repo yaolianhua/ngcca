f2d5093dac2a374d2b71b92c9330f510ddd9d351
Jason <yaolianhua789@gmail.com>
2022-11-02 11:28:30 +0800

    [security] log

--- hotcloud-security/hotcloud-security-server/src/main/java/io/hotcloud/security/server/configure/NoneSecurityConfigurer.java
+++ hotcloud-security/hotcloud-security-server/src/main/java/io/hotcloud/security/server/configure/NoneSecurityConfigurer.java
@@ -1,10 +1,13 @@
 package io.hotcloud.security.server.configure;
 
+import io.hotcloud.common.api.Log;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author yaolianhua789@gmail.com
@@ -21,6 +24,11 @@ public class NoneSecurityConfigurer {
         http.authorizeRequests().anyRequest().permitAll();
 
         return http.build();
+    }
+
+    @PostConstruct
+    public void print() {
+        Log.info(NoneSecurityConfigurer.class.getName(), "【Spring security disabled. if you want to enable, you need configure the environment 'security.enabled=true'】");
     }
 
 }
