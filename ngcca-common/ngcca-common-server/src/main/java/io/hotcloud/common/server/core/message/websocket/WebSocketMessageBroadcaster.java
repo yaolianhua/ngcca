99893179256104f65270b4a5db753ebb954efaf9
Jason <yaolianhua789@gmail.com>
2022-11-08 11:24:19 +0800

    [common] 固定rabbitmq 配置

--- ngcca-common/ngcca-common-server/src/main/java/io/hotcloud/common/server/core/message/websocket/WebSocketMessageBroadcaster.java
+++ ngcca-common/ngcca-common-server/src/main/java/io/hotcloud/common/server/core/message/websocket/WebSocketMessageBroadcaster.java
@@ -2,16 +2,18 @@ package io.hotcloud.common.server.core.message.websocket;
 
 import io.hotcloud.common.api.core.message.Message;
 import io.hotcloud.common.api.core.message.MessageBroadcaster;
+import io.hotcloud.common.server.core.message.rabbitmq.RabbitmqMessageBroadcaster;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.stereotype.Component;
 
 import javax.websocket.EncodeException;
 import javax.websocket.Session;
 import java.io.IOException;
 import java.util.Set;
 
-//TODO
-//@Component
-//@ConditionalOnMissingBean(RabbitmqMessageBroadcaster.class)
+@Component
+@ConditionalOnMissingBean(RabbitmqMessageBroadcaster.class)
 @Slf4j
 public class WebSocketMessageBroadcaster implements MessageBroadcaster {
 
