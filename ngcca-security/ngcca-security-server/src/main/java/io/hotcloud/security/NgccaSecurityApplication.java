6a9d5bd861a68cf7ed4b41fdbba6314fa11273f3
Jason <yaolianhua789@gmail.com>
2022-11-08 15:46:09 +0800

    [security] 优化 NgccaSecurityApplication

--- ngcca-security/ngcca-security-server/src/main/java/io/hotcloud/security/NgccaSecurityApplication.java
+++ ngcca-security/ngcca-security-server/src/main/java/io/hotcloud/security/NgccaSecurityApplication.java
@@ -1,9 +1,12 @@
 package io.hotcloud.security;
 
+import io.hotcloud.db.MongodbConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"io.hotcloud.db", "io.hotcloud.common", "io.hotcloud.security"})
+@SpringBootApplication(
+        scanBasePackageClasses = {MongodbConfiguration.class, NgccaSecurityApplication.class}
+)
 public class NgccaSecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(NgccaSecurityApplication.class, args);
