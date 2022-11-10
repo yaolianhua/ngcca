8d22345b4fdd3d22a0aba0b97811cd609aca59e6
Jason <yaolianhua789@gmail.com>
2022-11-10 17:27:35 +0800

    [kubernetes] 单独部署

---
+++ ngcca-kubernetes/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+REPOSITORY="harbor.local:5000/ngcca"
+BUILD_TIMESTAMP=$(date '+%Y%m%d%H%M%S')
+IMAGE="$REPOSITORY/k8s-agent:$BUILD_TIMESTAMP"
+
+docker build -t "${IMAGE}" .
+docker push "${IMAGE}"
