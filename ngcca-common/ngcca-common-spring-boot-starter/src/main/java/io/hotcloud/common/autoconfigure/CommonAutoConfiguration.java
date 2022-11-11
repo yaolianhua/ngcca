ee053bda9daf12018ad974494f6966798d34a30e
Jason <yaolianhua789@gmail.com>
2022-11-11 10:17:12 +0800

    [common] 迁移 AsyncConfiguration.java

--- ngcca-common/ngcca-common-spring-boot-starter/src/main/java/io/hotcloud/common/autoconfigure/CommonAutoConfiguration.java
+++ ngcca-common/ngcca-common-spring-boot-starter/src/main/java/io/hotcloud/common/autoconfigure/CommonAutoConfiguration.java
@@ -7,10 +7,21 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.task.TaskExecutorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+
+/**
+ * <a href="https://docs.spring.io/spring-boot/docs/current/reference/html/features.html#features.task-execution-and-scheduling">Task Execution and Scheduling</a>
+ */
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties({
         RegistryProperties.class,
@@ -21,6 +32,8 @@ import org.springframework.context.annotation.Import;
         MinioConfiguration.class
 })
 @EnableRabbit
+@EnableAsync
+@EnableScheduling
 public class CommonAutoConfiguration {
 
     @Bean
@@ -29,5 +42,26 @@ public class CommonAutoConfiguration {
         RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
         rabbitAdmin.setAutoStartup(true);
         return rabbitAdmin;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor(TaskExecutorBuilder taskExecutorBuilder) {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = taskExecutorBuilder.threadNamePrefix("common-").build();
+        threadPoolTaskExecutor.initialize();
+        return threadPoolTaskExecutor;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ExecutorService executorService(ThreadPoolTaskExecutor threadPoolTaskExecutor) {
+        return threadPoolTaskExecutor.getThreadPoolExecutor();
+    }
+
+    @Primary
+    @Bean
+    @ConditionalOnMissingBean
+    public Executor executor(ThreadPoolTaskExecutor threadPoolTaskExecutor) {
+        return threadPoolTaskExecutor;
     }
 }
