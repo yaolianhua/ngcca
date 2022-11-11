edf366739addddd2b5db0650a6863f2d8c46298a
Jason <yaolianhua789@gmail.com>
2022-11-11 10:20:55 +0800

    [serverside] NGCCAApplication

---
+++ ngcca-allinone-serverside/src/main/java/io/hotcloud/allinone/NGCCAApplication.java
@@ -0,0 +1,25 @@
+package io.hotcloud.allinone;
+
+import io.hotcloud.application.NgccaApplicationRootPackage;
+import io.hotcloud.buildpack.NgccaBuildPackRootPackage;
+import io.hotcloud.common.NgccaCommonRootPackage;
+import io.hotcloud.db.NgccaMongodbConfiguration;
+import io.hotcloud.kubernetes.client.EnableKubernetesAgentClient;
+import io.hotcloud.security.NgccaSecurityRootPackage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackageClasses = {
+        NgccaApplicationRootPackage.class,
+        NgccaBuildPackRootPackage.class,
+        NgccaSecurityRootPackage.class,
+        NgccaCommonRootPackage.class,
+        NgccaMongodbConfiguration.class
+})
+@EnableKubernetesAgentClient
+public class NGCCAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NGCCAApplication.class, args);
+    }
+}
