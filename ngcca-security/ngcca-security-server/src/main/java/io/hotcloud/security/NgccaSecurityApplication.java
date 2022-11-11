18493aa4b5fde3d6d4d97f7e9a564764c3ca667a
Jason <yaolianhua789@gmail.com>
2022-11-11 10:18:06 +0800

    [security] rename NgccaMongodbConfiguration

--- ngcca-security/ngcca-security-server/src/main/java/io/hotcloud/security/NgccaSecurityApplication.java
+++ ngcca-security/ngcca-security-server/src/main/java/io/hotcloud/security/NgccaSecurityApplication.java
@@ -1,11 +1,11 @@
 package io.hotcloud.security;
 
-import io.hotcloud.db.MongodbConfiguration;
+import io.hotcloud.db.NgccaMongodbConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(
-        scanBasePackageClasses = {MongodbConfiguration.class, NgccaSecurityApplication.class}
+        scanBasePackageClasses = {NgccaMongodbConfiguration.class, NgccaSecurityApplication.class}
 )
 public class NgccaSecurityApplication {
     public static void main(String[] args) {
