ae5916a17edd626f52446f15a94a13ab97b1270d
Jason <yaolianhua789@gmail.com>
2022-11-11 16:25:55 +0800

    [security] 优化配置

--- ngcca-security/ngcca-security-spring-boot-starter/src/main/java/io/hotcloud/security/autoconfigure/SecureWhitelistProperties.java
+++ ngcca-security/ngcca-security-spring-boot-starter/src/main/java/io/hotcloud/security/autoconfigure/SecureWhitelistProperties.java
@@ -18,8 +18,21 @@ public class SecureWhitelistProperties {
 
     private List<String> urls = new LinkedList<>();
 
+    private List<String> defaults = List.of(
+            "/swagger**/**",
+            "/v3/api-docs/**",
+            "/favicon.ico",
+            "/pub",
+            "/**/login"
+    );
+
     @PostConstruct
     public void print() {
-        Log.info(SecureWhitelistProperties.class.getName(), String.format("【Load SecureWhitelist Properties】ignored urls %s", urls));
+        Log.info(SecureWhitelistProperties.class.getName(), String.format("【Load SecureWhitelist Properties】ignored urls %s", getUrls()));
+    }
+
+    public List<String> getUrls() {
+        urls.addAll(defaults);
+        return urls;
     }
 }
