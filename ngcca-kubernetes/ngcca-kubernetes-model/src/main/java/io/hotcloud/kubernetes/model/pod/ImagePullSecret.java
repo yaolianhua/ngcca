e20d352c5a4075d0a332caaf1c3d632ec4458d99
Jason <yaolianhua789@gmail.com>
2022-11-16 15:13:31 +0800

    [kubernetes] 序列化问题

--- ngcca-kubernetes/ngcca-kubernetes-model/src/main/java/io/hotcloud/kubernetes/model/pod/ImagePullSecret.java
+++ ngcca-kubernetes/ngcca-kubernetes-model/src/main/java/io/hotcloud/kubernetes/model/pod/ImagePullSecret.java
@@ -1,7 +1,10 @@
 package io.hotcloud.kubernetes.model.pod;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
+@NoArgsConstructor
 public class ImagePullSecret {
 
     private String name;
