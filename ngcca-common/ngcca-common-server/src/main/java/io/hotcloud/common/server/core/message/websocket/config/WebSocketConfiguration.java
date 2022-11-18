5ee99d20ec1ec80f22cdbaad63996f1ac06b3a21
Jason <yaolianhua789@gmail.com>
2022-11-18 18:36:21 +0800

    [common] 不注入websocket

--- ngcca-common/ngcca-common-server/src/main/java/io/hotcloud/common/server/core/message/websocket/config/WebSocketConfiguration.java
+++ ngcca-common/ngcca-common-server/src/main/java/io/hotcloud/common/server/core/message/websocket/config/WebSocketConfiguration.java
@@ -1,16 +1,16 @@
 package io.hotcloud.common.server.core.message.websocket.config;
 
+import io.hotcloud.common.server.core.message.rabbitmq.RabbitmqMessageBroadcaster;
 import io.hotcloud.common.server.core.message.websocket.WebSocketMessageBroadcaster;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-/**
- * @author yaolianhua789@gmail.com
- **/
 @Configuration(proxyBeanMethods = false)
+@ConditionalOnMissingBean(RabbitmqMessageBroadcaster.class)
 @Slf4j
 public class WebSocketConfiguration {
 
