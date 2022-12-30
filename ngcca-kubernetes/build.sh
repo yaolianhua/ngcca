4562ea0ddc532cfc96cf240d3e9ebc272a25008c
Jason <yaolianhua789@gmail.com>
2022-12-30 21:32:11 +0800

    [kubernetes] 更新build.sh

--- ngcca-kubernetes/build.sh
+++ ngcca-kubernetes/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+namespace=$1
+if [[ -z $namespace ]]; then
+    namespace="harbor.local:5000"
+    echo "used registry url '$namespace'"
+fi
+
 mvn clean package
 
-REPOSITORY="harbor.local:5000/ngcca"
-BUILD_TIMESTAMP=$(date '+%Y%m%d%H%M%S')
-IMAGE="$REPOSITORY/k8s-agent:$BUILD_TIMESTAMP"
+IMAGE="$namespace/ngcca/k8s-agent:$(date '+%Y%m%d%H%M%S')"
 
 docker build -t "${IMAGE}" .
 docker push "${IMAGE}"
