16d97512f6a8204ca0f62de27376795ee673b52e
Jason <yaolianhua789@gmail.com>
2023-05-24 11:36:11 +0800

    [kubernetes] 调整构建脚本

--- build.sh
+++ build.sh
@@ -1,14 +1,51 @@
 #!/bin/bash
 
-namespace=$1
-if [[ -z $namespace ]]; then
-    namespace="harbor.local:5000"
-    echo "used local default registry '$namespace'"
+show_help_and_exit() {
+  cat << EOF
+Usage: $0 <project>
+
+projects:
+  kubernetes-agent                kubernetes-agent server service
+  core-server                     core server service
+  web                             web server service
+
+demo：
+  $0 kubernetes-agent
+  $0 core-server
+  $0 web
+
+EOF
+  exit 0
+}
+
+
+build_kubernetes_agent(){
+    echo "------ commit id ------"
+    printf "%s$(git rev-parse HEAD) \n"
+
+    echo "------ jar build ------"
+    mvn --batch-mode --errors --fail-fast --threads 1C --projects "io.hotcloud:ngcca-kubernetes-starter" --also-make clean package
+
+    IMAGE="harbor.local:5000/ngcca/kubernetes-agent:$(date '+%Y.%m.%d.%H%M%S')"
+
+    echo "------ docker build ------"
+    docker build -f ngcca-kubernetes/Dockerfile -t "${IMAGE}" .
+
+    echo "------ docker push ------"
+    docker push "${IMAGE}"
+}
+
+project=$1
+if [ -z "$project" ]; then
+    show_help_and_exit
 fi
 
-mvn --batch-mode --errors --fail-fast --threads 1C clean package
-
-IMAGE="$namespace/ngcca/ngcca-server:$(date '+%Y%m%d%H%M%S')"
-
-docker build -t "${IMAGE}" .
-docker push "${IMAGE}"
+case $project in
+kubernetes-agent)
+  build_kubernetes_agent
+  ;;
+*)
+  printf "unsupported project '%s$project' \n"
+  show_help_and_exit
+  ;;
+esac
