657d1d576c9be3c908dae6e1b6cb1b2b2f4f527f
Jason <yaolianhua789@gmail.com>
2023-06-01 13:57:53 +0800

    [web] 重命名

--- ngcca-web/src/main/java/io/hotcloud/web/controller/rest/UserManageRestController.java
+++ ngcca-web/src/main/java/io/hotcloud/web/controller/rest/UserManageRestController.java
@@ -18,13 +18,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/security/users")
 @Tag(name = "Security user")
-public class AdministratorUserManageRestController {
+public class UserManageRestController {
 
     private final UserApi userApi;
     private final UserCollectionQuery collectionQuery;
 
-    public AdministratorUserManageRestController(UserApi userApi,
-                                                 UserCollectionQuery collectionQuery) {
+    public UserManageRestController(UserApi userApi,
+                                    UserCollectionQuery collectionQuery) {
         this.userApi = userApi;
         this.collectionQuery = collectionQuery;
     }
