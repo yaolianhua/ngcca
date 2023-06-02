50308a6c02317afa33d3e35af6f0ca009075d65c
Jason <yaolianhua789@gmail.com>
2023-06-02 17:49:14 +0800

    [vendor-registry] 出事话dockerClient

--- ngcca-vendors/ngcca-vendor-registry/src/main/java/io/hotcloud/vendor/registry/RegistryConfiguration.java
+++ ngcca-vendors/ngcca-vendor-registry/src/main/java/io/hotcloud/vendor/registry/RegistryConfiguration.java
@@ -1,7 +1,47 @@
 package io.hotcloud.vendor.registry;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
+import io.hotcloud.common.log.Event;
+import io.hotcloud.common.log.Log;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
+
 @Configuration(proxyBeanMethods = false)
+@EnableConfigurationProperties(value = {
+        DockerProperties.class
+})
 public class RegistryConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public DockerClient dockerClient(DockerProperties properties) {
+
+        DefaultDockerClientConfig dockerClientConfig = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withDockerHost(properties.getHost())
+                .withDockerTlsVerify(false)
+                .withRegistryUrl(properties.getRegistryUrl())
+                .withRegistryUsername(properties.getRegistryUsername())
+                .withRegistryPassword(properties.getRegistryPassword())
+                .build();
+
+        DockerHttpClient dockerHttpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(dockerClientConfig.getDockerHost())
+                .sslConfig(dockerClientConfig.getSSLConfig())
+                .maxConnections(100)
+                .connectionTimeout(Duration.ofSeconds(30))
+                .responseTimeout(Duration.ofSeconds(45))
+                .build();
+
+        DockerClient dockerClient = DockerClientImpl.getInstance(dockerClientConfig, dockerHttpClient);
+        Log.info(this, properties, Event.START, "load docker client configuration");
+        return dockerClient;
+    }
 }
