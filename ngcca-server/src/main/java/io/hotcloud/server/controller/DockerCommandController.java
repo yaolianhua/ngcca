6413a845011506e8140c7583259202602b07772d
Jason <yaolianhua789@gmail.com>
2023-06-05 20:48:48 +0800

    [server] docker pull api

---
+++ ngcca-server/src/main/java/io/hotcloud/server/controller/DockerCommandController.java
@@ -0,0 +1,42 @@
+package io.hotcloud.server.controller;
+
+import io.hotcloud.common.model.Result;
+import io.hotcloud.vendor.registry.client.DockerPullClient;
+import io.hotcloud.vendor.registry.client.DockerPushClient;
+import io.hotcloud.vendor.registry.model.RegistryImage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static io.hotcloud.common.model.WebResponse.ok;
+
+
+@RestController
+@RequestMapping("/v1/docker/cmd")
+@Tag(name = "Docker command")
+public class DockerCommandController {
+
+    private final DockerPullClient dockerPullClient;
+    private final DockerPushClient dockerPushClient;
+
+    public DockerCommandController(DockerPullClient dockerPullClient, DockerPushClient dockerPushClient) {
+        this.dockerPullClient = dockerPullClient;
+        this.dockerPushClient = dockerPushClient;
+    }
+
+    @PostMapping("/pull")
+    @Operation(
+            summary = "pull docker image",
+            responses = {@ApiResponse(responseCode = "200")},
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "docker pull request body")
+    )
+    public ResponseEntity<Result<Boolean>> pull(@RequestBody RegistryImage body) {
+        return ok(dockerPullClient.pull(body));
+    }
+
+}
