ca86b8f99e5a3733d182fb9296ef16ac2796fc38
Jason <yaolianhua789@gmail.com>
2023-07-02 17:34:37 +0800

    [web] 清空日志

---
+++ ngcca-web/src/main/java/io/hotcloud/web/controller/rest/ActivityController.java
@@ -0,0 +1,35 @@
+package io.hotcloud.web.controller.rest;
+
+import io.hotcloud.module.db.entity.ActivityRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/activities")
+@Tag(name = "Activity")
+@CrossOrigin
+public class ActivityController {
+
+
+    private final ActivityRepository activityRepository;
+
+    public ActivityController(ActivityRepository activityRepository) {
+        this.activityRepository = activityRepository;
+    }
+
+    @DeleteMapping
+    @Operation(
+            summary = "delete all activity",
+            responses = {@ApiResponse(responseCode = "200")}
+    )
+    public ResponseEntity<Void> delete() {
+        activityRepository.deleteAll();
+        return ResponseEntity.ok(null);
+    }
+}
--- ngcca-web/src/main/resources/static/js/activity-list.js
+++ ngcca-web/src/main/resources/static/js/activity-list.js
@@ -1,4 +1,18 @@
+const ACTIVITY_API = "/v1/activities";
+
 $(function () {
+    pagelist();
+});
+
+const swal = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+})
+
+function pagelist() {
     //分页
     $('#activity-list').DataTable({
         "paging": true,
@@ -9,4 +23,28 @@ $(function () {
         "autoWidth": true,
         "responsive": true,
     });
-});
+}
+
+function activityempty() {
+    swal.fire({
+        title: '确认删除?',
+        text: '所有活动日志都会被清除，谨慎操作!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: '确认',
+        cancelButtonText: '取消',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            axios.delete(ACTIVITY_API)
+                .then(response => {
+                    window.location.href = "/administrator/activity/activity-list";
+                })
+                .catch(error => {
+                    fail(error);
+                });
+        } else {
+            //
+        }
+    })
+}
