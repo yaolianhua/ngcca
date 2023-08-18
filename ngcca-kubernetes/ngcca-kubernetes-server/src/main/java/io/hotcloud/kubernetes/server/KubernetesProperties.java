9bce62e80666c2771cd1d3e94e302afb2a850e27
Jason <yaolianhua789@gmail.com>
2023-08-18 19:10:24 +0800

    [kubernetes] 调整配置，使用配置中心配置

--- ngcca-kubernetes/ngcca-kubernetes-server/src/main/java/io/hotcloud/kubernetes/server/KubernetesProperties.java
+++ ngcca-kubernetes/ngcca-kubernetes-server/src/main/java/io/hotcloud/kubernetes/server/KubernetesProperties.java
@@ -2,14 +2,18 @@ package io.hotcloud.kubernetes.server;
 
 import io.hotcloud.common.log.Event;
 import io.hotcloud.common.log.Log;
+import io.hotcloud.common.model.Properties;
 import jakarta.annotation.PostConstruct;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import static io.hotcloud.common.model.CommonConstant.CONFIG_PREFIX;
+
 @Configuration(proxyBeanMethods = false)
 @ConfigurationProperties(prefix = "kubernetes")
 @Data
+@Properties(prefix = CONFIG_PREFIX + "kubernetes")
 public class KubernetesProperties {
 
     public static final String ENABLE_WORKLOADS_WATCHER = "kubernetes.enable-workloads-watcher";
