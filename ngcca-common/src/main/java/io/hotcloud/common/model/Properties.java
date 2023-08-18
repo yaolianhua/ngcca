7c4002fdb4af15b4f551aa74eb62f51bb03aa7ba
Jason <yaolianhua789@gmail.com>
2023-08-18 18:54:45 +0800

    [overall] nacos配置中心

--- ngcca-common/src/main/java/io/hotcloud/common/model/Properties.java
+++ ngcca-common/src/main/java/io/hotcloud/common/model/Properties.java
@@ -1,10 +1,13 @@
 package io.hotcloud.common.model;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.FIELD})
 @Documented
+@RefreshScope
 public @interface Properties {
     String prefix() default "";
 }
