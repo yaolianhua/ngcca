de9268610413c9642912dc60a650e52a96a414d6
Jason <yaolianhua789@gmail.com>
2023-09-01 13:17:21 +0800

    [service] 移除定时任务相关配置

--- ngcca-service/src/main/java/io/hotcloud/service/ServiceModuleAutoConfiguration.java
+++ ngcca-service/src/main/java/io/hotcloud/service/ServiceModuleAutoConfiguration.java
@@ -8,19 +8,9 @@ import io.hotcloud.service.registry.SystemRegistryImageProperties;
 import io.hotcloud.service.registry.SystemRegistryProperties;
 import io.hotcloud.service.security.SecurityConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.task.TaskExecutorBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 
 @AutoConfiguration
 @ComponentScan
@@ -35,32 +25,6 @@ import java.util.concurrent.ExecutorService;
         OpenAiConfiguration.class,
         SecurityConfiguration.class
 })
-@EnableAsync
-@EnableScheduling
 public class ServiceModuleAutoConfiguration {
 
-
-    /**
-     * <a href="https://docs.spring.io/spring-boot/docs/current/reference/html/features.html#features.task-execution-and-scheduling">Task Execution and Scheduling</a>
-     */
-    @Bean
-    @ConditionalOnMissingBean
-    public ThreadPoolTaskExecutor threadPoolTaskExecutor(TaskExecutorBuilder taskExecutorBuilder) {
-        ThreadPoolTaskExecutor threadPoolTaskExecutor = taskExecutorBuilder.threadNamePrefix("common-").build();
-        threadPoolTaskExecutor.initialize();
-        return threadPoolTaskExecutor;
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public ExecutorService executorService(ThreadPoolTaskExecutor threadPoolTaskExecutor) {
-        return threadPoolTaskExecutor.getThreadPoolExecutor();
-    }
-
-    @Primary
-    @Bean
-    @ConditionalOnMissingBean
-    public Executor executor(ThreadPoolTaskExecutor threadPoolTaskExecutor) {
-        return threadPoolTaskExecutor;
-    }
 }
