b5f87ae390977bac3ccda6cabcf215a152ebaf4d
Jason <yaolianhua789@gmail.com>
2023-09-06 09:51:23 +0800

    [common] add SpringBeanUtil

---
+++ ngcca-common/src/main/java/io/hotcloud/common/utils/SpringBeanUtil.java
@@ -0,0 +1,27 @@
+package io.hotcloud.common.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanUtil implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    public static <T> T getBean(Class<T> beanClass) {
+        return applicationContext.getBean(beanClass);
+    }
+
+    public static <T> T getBean(String beanName, Class<T> beanClass) {
+        return applicationContext.getBean(beanName, beanClass);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringBeanUtil.applicationContext = applicationContext;
+    }
+
+    //
+}
