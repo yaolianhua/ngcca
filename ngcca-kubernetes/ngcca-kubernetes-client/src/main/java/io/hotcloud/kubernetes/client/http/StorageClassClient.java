a7e7c7349403163f3ff1e60cd625e62c7d38c6d9
Jason <yaolianhua789@gmail.com>
2023-10-09 13:44:20 +0800

    [kubernetes] StorageClassClient支持集群模式

--- ngcca-kubernetes/ngcca-kubernetes-client/src/main/java/io/hotcloud/kubernetes/client/http/StorageClassClient.java
+++ ngcca-kubernetes/ngcca-kubernetes-client/src/main/java/io/hotcloud/kubernetes/client/http/StorageClassClient.java
@@ -23,6 +23,15 @@ public interface StorageClassClient {
     StorageClass create(StorageClassCreateRequest request) throws ApiException;
 
     /**
+     * Create StorageClass from {@code StorageClassCreateRequest}
+     *
+     * @param request {@link StorageClassCreateRequest}
+     * @return {@link StorageClass}
+     * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
+     */
+    StorageClass create(String agent, StorageClassCreateRequest request) throws ApiException;
+
+    /**
      * Create StorageClass from yaml
      *
      * @param yaml {@link YamlBody}
@@ -30,6 +39,15 @@ public interface StorageClassClient {
      * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
      */
     StorageClass create(YamlBody yaml) throws ApiException;
+
+    /**
+     * Create StorageClass from yaml
+     *
+     * @param yaml {@link YamlBody}
+     * @return {@link StorageClass}
+     * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
+     */
+    StorageClass create(String agent, YamlBody yaml) throws ApiException;
 
     /**
      * Delete named StorageClass
@@ -41,6 +59,15 @@ public interface StorageClassClient {
     Void delete(String storageClass) throws ApiException;
 
     /**
+     * Delete named StorageClass
+     *
+     * @param storageClass StorageClass name
+     * @return {@link Void}
+     * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
+     */
+    Void delete(String agent, String storageClass) throws ApiException;
+
+    /**
      * Read named StorageClass
      *
      * @param name StorageClass name
@@ -49,10 +76,26 @@ public interface StorageClassClient {
     StorageClass read(String name);
 
     /**
+     * Read named StorageClass
+     *
+     * @param name StorageClass name
+     * @return {@link StorageClass}
+     */
+    StorageClass read(String agent, String name);
+
+    /**
      * Read StorageClassList
      *
      * @param labelSelector label selector
      * @return {@link StorageClassList}
      */
     StorageClassList readList(Map<String, String> labelSelector);
+
+    /**
+     * Read StorageClassList
+     *
+     * @param labelSelector label selector
+     * @return {@link StorageClassList}
+     */
+    StorageClassList readList(String agent, Map<String, String> labelSelector);
 }
--- ngcca-kubernetes/ngcca-kubernetes-client/src/main/java/io/hotcloud/kubernetes/client/http/factory/StorageClassClientImpl.java
+++ ngcca-kubernetes/ngcca-kubernetes-client/src/main/java/io/hotcloud/kubernetes/client/http/factory/StorageClassClientImpl.java
@@ -30,11 +30,12 @@ import java.util.Objects;
 class StorageClassClientImpl implements StorageClassClient {
     private final URI uri;
     private final RestTemplate restTemplate;
+    private static final String API = "/v1/kubernetes/storageclasses";
 
     public StorageClassClientImpl(KubernetesAgentProperties clientProperties,
                                   RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-        uri = URI.create(clientProperties.getAgentHttpUrl() + "/v1/kubernetes/storageclasses");
+        uri = URI.create(clientProperties.getAgentHttpUrl() + API);
     }
 
     @Override
@@ -42,6 +43,17 @@ class StorageClassClientImpl implements StorageClassClient {
         RequestParamAssertion.assertBodyNotNull(request);
 
         ResponseEntity<StorageClass> response = restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(request),
+                new ParameterizedTypeReference<>() {
+                });
+
+        return response.getBody();
+    }
+
+    @Override
+    public StorageClass create(String agent, StorageClassCreateRequest request) throws ApiException {
+        RequestParamAssertion.assertBodyNotNull(request);
+
+        ResponseEntity<StorageClass> response = restTemplate.exchange(URI.create(agent + API), HttpMethod.POST, new HttpEntity<>(request),
                 new ParameterizedTypeReference<>() {
                 });
 
@@ -64,12 +76,41 @@ class StorageClassClientImpl implements StorageClassClient {
     }
 
     @Override
+    public StorageClass create(String agent, YamlBody yaml) throws ApiException {
+        RequestParamAssertion.assertBodyNotNull(yaml);
+        Assert.hasText(yaml.getYaml(), "yaml content is null");
+
+        URI uriRequest = UriComponentsBuilder
+                .fromHttpUrl(String.format("%s/yaml", URI.create(agent + API)))
+                .build().toUri();
+        ResponseEntity<StorageClass> response = restTemplate.exchange(uriRequest, HttpMethod.POST, new HttpEntity<>(yaml),
+                new ParameterizedTypeReference<>() {
+                });
+
+        return response.getBody();
+    }
+
+    @Override
     public Void delete(String storageClass) throws ApiException {
 
         RequestParamAssertion.assertResourceNameNotNull(storageClass);
 
         URI uriRequest = UriComponentsBuilder
                 .fromHttpUrl(String.format("%s/{name}", uri.toString()))
+                .build(storageClass);
+
+        ResponseEntity<Void> response = restTemplate.exchange(uriRequest, HttpMethod.DELETE, HttpEntity.EMPTY,
+                new ParameterizedTypeReference<>() {
+                });
+        return response.getBody();
+    }
+
+    @Override
+    public Void delete(String agent, String storageClass) throws ApiException {
+        RequestParamAssertion.assertResourceNameNotNull(storageClass);
+
+        URI uriRequest = UriComponentsBuilder
+                .fromHttpUrl(String.format("%s/{name}", URI.create(agent + API)))
                 .build(storageClass);
 
         ResponseEntity<Void> response = restTemplate.exchange(uriRequest, HttpMethod.DELETE, HttpEntity.EMPTY,
@@ -95,6 +136,21 @@ class StorageClassClientImpl implements StorageClassClient {
     }
 
     @Override
+    public StorageClass read(String agent, String name) {
+        RequestParamAssertion.assertResourceNameNotNull(name);
+
+        URI uriRequest = UriComponentsBuilder
+                .fromHttpUrl(String.format("%s/{name}", URI.create(agent + API)))
+                .build(name);
+
+        ResponseEntity<StorageClass> response = restTemplate.exchange(uriRequest, HttpMethod.GET, HttpEntity.EMPTY,
+                new ParameterizedTypeReference<>() {
+                });
+
+        return response.getBody();
+    }
+
+    @Override
     public StorageClassList readList(Map<String, String> labelSelector) {
         labelSelector = Objects.isNull(labelSelector) ? Map.of() : labelSelector;
 
@@ -103,6 +159,24 @@ class StorageClassClientImpl implements StorageClassClient {
 
         URI uriRequest = UriComponentsBuilder
                 .fromHttpUrl(uri.toString())
+                .queryParams(params)
+                .build().toUri();
+
+        ResponseEntity<StorageClassList> response = restTemplate.exchange(uriRequest, HttpMethod.GET, HttpEntity.EMPTY,
+                new ParameterizedTypeReference<>() {
+                });
+        return response.getBody();
+    }
+
+    @Override
+    public StorageClassList readList(String agent, Map<String, String> labelSelector) {
+        labelSelector = Objects.isNull(labelSelector) ? Map.of() : labelSelector;
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        labelSelector.forEach(params::add);
+
+        URI uriRequest = UriComponentsBuilder
+                .fromHttpUrl(URI.create(agent + API).toString())
                 .queryParams(params)
                 .build().toUri();
 
