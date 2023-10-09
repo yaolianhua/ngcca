9444abd5b0d51c962a5a8cdefd3b8395708f9bed
Jason <yaolianhua789@gmail.com>
2023-10-09 10:17:43 +0800

    [kubernetes] ConfigMapClient支持集群模式

--- ngcca-kubernetes/ngcca-kubernetes-client/src/main/java/io/hotcloud/kubernetes/client/http/ConfigMapClient.java
+++ ngcca-kubernetes/ngcca-kubernetes-client/src/main/java/io/hotcloud/kubernetes/client/http/ConfigMapClient.java
@@ -23,6 +23,15 @@ public interface ConfigMapClient {
     ConfigMap read(String namespace, String configmap);
 
     /**
+     * Read namespaced ConfigMap
+     *
+     * @param namespace namespace
+     * @param configmap configmap name
+     * @return {@link ConfigMap}
+     */
+    ConfigMap read(String agentUrl, String namespace, String configmap);
+
+    /**
      * Read namespaced ConfigMapList
      *
      * @param namespace     namespace
@@ -32,11 +41,27 @@ public interface ConfigMapClient {
     ConfigMapList readList(String namespace, Map<String, String> labelSelector);
 
     /**
+     * Read namespaced ConfigMapList
+     *
+     * @param namespace     namespace
+     * @param labelSelector label selector
+     * @return {@link ConfigMapList}
+     */
+    ConfigMapList readList(String agentUrl, String namespace, Map<String, String> labelSelector);
+
+    /**
      * Read all namespaced ConfigMapList
      *
      * @return {@link ConfigMapList}
      */
     ConfigMapList readList();
+
+    /**
+     * Read all namespaced ConfigMapList
+     *
+     * @return {@link ConfigMapList}
+     */
+    ConfigMapList readList(String agentUrl);
 
     /**
      * Create ConfigMap from {@code ConfigMapCreateRequest}
@@ -48,6 +73,15 @@ public interface ConfigMapClient {
     ConfigMap create(ConfigMapCreateRequest request) throws ApiException;
 
     /**
+     * Create ConfigMap from {@code ConfigMapCreateRequest}
+     *
+     * @param request {@link ConfigMapCreateRequest}
+     * @return {@link ConfigMap}
+     * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
+     */
+    ConfigMap create(String agentUrl, ConfigMapCreateRequest request) throws ApiException;
+
+    /**
      * Create ConfigMap from {@code YamlBody}
      *
      * @param yaml {@link YamlBody}
@@ -55,6 +89,15 @@ public interface ConfigMapClient {
      * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
      */
     ConfigMap create(YamlBody yaml) throws ApiException;
+
+    /**
+     * Create ConfigMap from {@code YamlBody}
+     *
+     * @param yaml {@link YamlBody}
+     * @return {@link ConfigMap}
+     * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
+     */
+    ConfigMap create(String agentUrl, YamlBody yaml) throws ApiException;
 
     /**
      * Delete namespaced ConfigMap
@@ -65,5 +108,15 @@ public interface ConfigMapClient {
      * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
      */
     Void delete(String namespace, String configmap) throws ApiException;
+
+    /**
+     * Delete namespaced ConfigMap
+     *
+     * @param namespace namespace
+     * @param configmap configmap name
+     * @return {@link Void}
+     * @throws ApiException throws {@code ApiException} if the request could not be processed correctly from k8s api server
+     */
+    Void delete(String agentUrl, String namespace, String configmap) throws ApiException;
 
 }
