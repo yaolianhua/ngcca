b08770e341887144192004f4495de1db09cd0701
Jason <yaolianhua789@gmail.com>
2023-11-06 13:32:07 +0800

    [web] 优化用户应用列表codemirror展示

--- ngcca-web/src/main/java/io/hotcloud/web/views/UserViews.java
+++ ngcca-web/src/main/java/io/hotcloud/web/views/UserViews.java
@@ -6,7 +6,7 @@ public final class UserViews {
     public static final String INSTANCE_LIST = "user/template/instance-list";
     public static final String USER_APPLICATION = "user/application/application-list";
     public static final String INSTANCE_LIST_FRAGMENT = "user/template/instance-list::content";
-    public static final String USER_APPLICATION_LIST_FRAGMENT = "user/application/application-list-fragment::content";
+    public static final String USER_APPLICATION_LIST_FRAGMENT = "user/application/application-list::content";
 
     public static final String INDEX = "index";
     public static final String REDIRECT_INDEX = "redirect:/index";
