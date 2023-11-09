2d07de0ac2bdfe9ef9c2bf62bc8e5c2bf84305c1
Jason <yaolianhua789@gmail.com>
2023-11-09 12:14:12 +0800

    [web] 补充校验 js

--- ngcca-web/src/main/resources/static/js/common.js
+++ ngcca-web/src/main/resources/static/js/common.js
@@ -60,6 +60,27 @@ function fail(error) {
     toastr.error('操作失败[' + error.response.data.message + ']');
 }
 
+function isEmpty(str) {
+    return str === "" ||
+        str === "null" ||
+        str === "undefined" ||
+        str == null;
+}
+
+function alertWarn(msg) {
+    animate_swal.fire({
+        icon: 'warning',
+        html: msg
+    })
+}
+
+function alertInfo(msg) {
+    animate_swal.fire({
+        icon: 'info',
+        html: msg
+    })
+}
+
 const animate_swal = Swal.mixin({
     showClass: {
         popup: 'animate__animated animate__fadeInDown'
