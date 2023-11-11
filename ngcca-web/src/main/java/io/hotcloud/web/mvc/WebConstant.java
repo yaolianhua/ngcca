362826580c264077e5e6451861b4cd60d1ca2e3a
Jason <yaolianhua789@gmail.com>
2023-11-11 13:34:17 +0800

    [web] 移除废弃常量

--- ngcca-web/src/main/java/io/hotcloud/web/mvc/WebConstant.java
+++ ngcca-web/src/main/java/io/hotcloud/web/mvc/WebConstant.java
@@ -1,24 +1,17 @@
 package io.hotcloud.web.mvc;
 
-/**
- * @author yaolianhua789@gmail.com
- **/
 public final class WebConstant {
 
     public static final String USER = "user";
-    public static final String TEMPLATE_INSTANCE = "templateInstance";
-    public static final String TEMPLATE_DEFINITION = "templateDefinition";
 
 
     public static final String SERVER_ENDPOINT = "endpoint";
-    public static final String RESPONSE = "response";
     public static final String PAGE = "page";
     public static final String COLLECTION = "collection";
     public static final String TEMPLATES = "templates";
     public static final String CLUSTERS = "clusters";
     public static final String TEMPLATE_LOGOS = "logos";
     public static final String STATISTICS = "statistics";
-    public static final String ACTIVITIES = "activities";
     public static final String MESSAGE = "message";
 
     public static final String VIEW_LIST_FRAGMENT = "list";
