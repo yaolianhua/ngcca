0426ba6eca8a52df47040ef791322ffafb0593a2
Jason <yaolianhua789@gmail.com>
2023-11-17 16:00:34 +0800

    [web] 优化 js

---
+++ ngcca-web/src/main/resources/static/js/auth.js
@@ -0,0 +1,31 @@
+//
+const WEB_ENDPOINT = $('#server-endpoint').data('server-endpoint');
+
+//axios authorization header set
+axios.defaults.baseURL = WEB_ENDPOINT;
+axios.defaults.withCredentials = true;
+// Request interceptors for API calls
+axios.interceptors.request.use(
+    config => {
+        config.headers['Authorization'] = `Bearer ${getAuthorization()}`;
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+//Get authorization from cookies
+function getAuthorization() {
+    let strcookie = document.cookie;//获取cookie字符串
+    let arrcookie = strcookie.split("; ");//分割
+    //遍历匹配
+    for (let i = 0; i < arrcookie.length; i++) {
+        let arr = arrcookie[i].split("=");
+        if (arr[0] === "authorization") {
+            return arr[1];
+        }
+    }
+    return "";
+}
+
